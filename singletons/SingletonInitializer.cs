c80be0538428622a09229fe673b20bce452b823b
RSHAW <scrudgey@gmail.com>
2015-10-18 00:08:43 -0700

    added robust load routine`

--- singletons/SingletonInitializer.cs
+++ singletons/SingletonInitializer.cs
@@ -6,11 +6,42 @@ public class SingletonInitializer : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		string tempstring;
-		tempstring = GameManager.Instance.message;
 		tempstring = Toolbox.Instance.message;
+		RobustLoad();
+		tempstring = GameManager.Instance.message;
 		tempstring = Messenger.Instance.MOTD;
 		tempstring = UISystem.Instance.MOTD;
 		Debug.Log(tempstring);
 	}
 
+	public void RobustLoad(){
+		// this routine checks to make sure all the necessary pieces are instantiated in the scene.
+		// if they aren't, it will spawn them.
+		string[] requirements = new string[] {"singleton initializer", "UI", "Main Camera", "EventSystem"};
+		foreach(string requirement in requirements){
+			GameObject go = GameObject.Find(requirement);
+			if (!go){
+				Debug.Log("instantitating required "+requirement);
+				string path = @"required/"+requirement;
+				GameObject newgo = GameObject.Instantiate(Resources.Load(path)) as GameObject;
+				newgo.name = Toolbox.Instance.ScrubText(newgo.name);
+			}
+		}
+		
+		GameObject mainCamera = GameObject.Find("Main Camera");
+		Camera cam = mainCamera.GetComponent<Camera>();
+		if (mainCamera){
+			CameraControl cameraControl = mainCamera.GetComponent<CameraControl>();
+			if (!cameraControl)
+				cameraControl = mainCamera.AddComponent<CameraControl>();
+
+		}
+
+		GameObject ui = GameObject.Find("UI");
+		if (ui){
+			Canvas canvas = ui.GetComponent<Canvas>();
+			canvas.worldCamera = cam;
+		}
+		
+	}
 }
