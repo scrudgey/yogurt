cfdd5ebc096ced5ffab2bdcf029db6e1b1c9483f
RSHAW <scrudgey@gmail.com>
2015-10-20 22:08:24 -0700

    debug comment imapct damage

--- generics/Destructible.cs
+++ generics/Destructible.cs
@@ -20,7 +20,6 @@ public class Destructible : MonoBehaviour {
 
 	public void TakeDamage( Destructible.damageType type , float damage){
 		if (!invulnerable){
-
 			switch (type)
 			{
 			case damageType.physical:
@@ -38,8 +37,6 @@ public class Destructible : MonoBehaviour {
 			default:
 				break;
 			}
-
-
 		}
 	}
 
@@ -47,40 +44,35 @@ public class Destructible : MonoBehaviour {
 		foreach (Gibs gib in GetComponents<Gibs>())
 			if(gib.damageCondition == lastDamage || gib.damageCondition == damageType.any)
 				gib.Emit();
-
 		PhysicalBootstrapper phys = GetComponent<PhysicalBootstrapper>();
-
 		if (phys){
 			phys.DestroyPhysical();
 		}
-
 		if (destroySound.Length > 0){
 			GameObject speaker = Instantiate(Resources.Load("Speaker"),transform.position,Quaternion.identity) as GameObject;
 			speaker.GetComponent<AudioSource>().clip = destroySound[Random.Range(0,destroySound.Length)];
 			speaker.GetComponent<AudioSource>().Play();
 		}
-
 		LiquidContainer container = GetComponent<LiquidContainer>();
-
 		if (container && container.amount > 0){
 			Toolbox.Instance.SpawnDroplet(transform.position, container.liquid);
 			Toolbox.Instance.SpawnDroplet(transform.position, container.liquid);
 			Toolbox.Instance.SpawnDroplet(transform.position, container.liquid);
 		}
-
 		BroadcastMessage("OnDestruction",SendMessageOptions.DontRequireReceiver);
 		Messenger.Instance.WasDestroyed(gameObject);
 	}
 
 	void OnCollisionEnter2D(Collision2D col){
-
 		float vel = col.relativeVelocity.magnitude;
 		if (vel > 0.5){
 			//if we were hit hard, take a splatter damage
 			//i should fix this to be more physical
-			TakeDamage(damageType.physical,col.rigidbody.mass * vel*4);
+			float damage = col.rigidbody.mass * vel / 5.0f;
+			TakeDamage(damageType.physical, damage);
+			Debug.Log("Collision damage on " + gameObject.name + " to the tune of " + damage.ToString());
+			Debug.Log("Collided with "+col.gameObject.name);
 		}
-
 		if (hitSound.Length > 0){
 			GetComponent<AudioSource>().PlayOneShot(hitSound[Random.Range(0,hitSound.Length)]);
 		}
