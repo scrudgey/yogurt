f3371f460c9c56e522df426d77d3829f97a13751
RSHAW <scrudgey@gmail.com>
2015-10-25 19:24:30 -0700

    linting

--- itemcode/Slasher.cs
+++ itemcode/Slasher.cs
@@ -2,14 +2,11 @@
 using System.Collections;
 
 public class Slasher : MonoBehaviour {
-
 	public AudioClip[] impactSounds;
 	public Vector2 direction; 
-
-
+	
 	void SlashOn(){
 		GetComponent<Collider2D>().enabled = true;
-
 	}
 
 	void SlashOff(){
@@ -17,21 +14,16 @@ public class Slasher : MonoBehaviour {
 	}
 
 	void OnTriggerStay2D(Collider2D collider){
-
 		if (collider.gameObject.tag == "Physical"){
 			Physical phys = collider.gameObject.GetComponentInParent<Physical>();
 			if (phys){
 				if(phys.currentMode == Physical.mode.ground){
-//					Vector2 force = 100*Vector2.up + 250 * Vector2.right * transform.lossyScale.x;
-//					force = -100*Vector2.up;
-					Vector2 force = direction * 1500;
+					Vector2 force = direction;
 					// this is the important aspect that will need changing in order to apply forces correctly.
 					phys.Impact(force);
 				}
 			}
-
 		}
-		
 		LiquidContainer container = collider.GetComponent<LiquidContainer>();
 		if (container){
 			container.Spill();
@@ -39,7 +31,6 @@ public class Slasher : MonoBehaviour {
 	}
 
 	void OnTriggerEnter2D(Collider2D collider){
-
 		if (collider.gameObject.tag == "Physical" && impactSounds.Length > 0){
 			GetComponent<AudioSource>().PlayOneShot(impactSounds[Random.Range(0,impactSounds.Length)]);
 		}
