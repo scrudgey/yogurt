20005690071b654c5ee7eeabdf7406a2077acee3
RSHAW <scrudgey@gmail.com>
2015-10-25 18:38:51 -0700

    linting & updating audio call

--- generic behaviors/PlaySoundOnGroundImpact.cs
+++ generic behaviors/PlaySoundOnGroundImpact.cs
@@ -7,19 +7,14 @@ public class PlaySoundOnGroundImpact : MonoBehaviour {
 
 	void Start () {
 		if (groundImpactSounds.Length> 0){
-			if (!GetComponent<AudioSource>()){
-				gameObject.AddComponent<AudioSource>();
-			}
-			GetComponent<AudioSource>().rolloffMode = AudioRolloffMode.Logarithmic;
-			GetComponent<AudioSource>().minDistance = 0.4f;
-			GetComponent<AudioSource>().maxDistance = 5.42f;
+			Toolbox.Instance.SetUpAudioSource(gameObject);
 		}
 	}
 
 	public void OnGroundImpact(Physical phys){
 		if (groundImpactSounds.Length > 0){
-			GameObject speaker = Instantiate(Resources.Load("Speaker"),transform.position,Quaternion.identity) as GameObject;
-			speaker.GetComponent<AudioSource>().clip = groundImpactSounds[Random.Range(0,groundImpactSounds.Length)];
+			GameObject speaker = Instantiate(Resources.Load("Speaker"), transform.position, Quaternion.identity) as GameObject;
+			speaker.GetComponent<AudioSource>().clip = groundImpactSounds[Random.Range(0, groundImpactSounds.Length)];
 			speaker.GetComponent<AudioSource>().Play();
 		}
 	}
