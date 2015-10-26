aeb8f62aa2e641886b2459cd4d111b8d63aaebf6
RSHAW <scrudgey@gmail.com>
2015-10-25 18:46:15 -0700

    linting

--- human/Inventory.cs
+++ human/Inventory.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 public class Inventory : Interactive, IExcludable {
-
 	public List<GameObject> items;
 	public float strength;
 	public Pickup holding{
@@ -30,19 +28,14 @@ public class Inventory : Interactive, IExcludable {
 	private Transform holdpoint;
 	public GameObject slasher;
 	private string slashFlag;
-
 	private Controllable controllable;
 	private Interaction defaultInteraction;
-	
 	private List<Interaction> manualActionDictionary;
-
 	public bool swinging;
 	private bool LoadInitialized = false;
-
 	private GameObject throwObject;
-
-
-	void Start () {
+	
+	void Start(){
 		if (!LoadInitialized)
 			LoadInit();
 	}
@@ -50,12 +43,10 @@ public class Inventory : Interactive, IExcludable {
 	public void LoadInit(){
 		controllable = GetComponent<Controllable>();
 		holdpoint = transform.Find("holdpoint");
-		interactions.Add ( new Interaction(this, "Get", "GetItem",true,false));
-		
-		Interaction swingAction = new Interaction(this,"Swing","SwingItem",false,true);
+		interactions.Add(new Interaction(this, "Get", "GetItem",true,false));
+		Interaction swingAction = new Interaction(this, "Swing", "SwingItem", false,true);
 		swingAction.defaultPriority = 5;
-		interactions.Add (swingAction);
-
+		interactions.Add(swingAction);
 		LoadInitialized = true;
 	}
 
@@ -71,14 +62,12 @@ public class Inventory : Interactive, IExcludable {
 				if (holding){
 					DropItem();
 				}
-
 				//make the object the current holding.
 				Messenger.Instance.ClaimObject(pickup.gameObject,this);
 				holding = pickup;
 				PhysicalBootstrapper phys = holding.GetComponent<PhysicalBootstrapper>();
 				if (phys)
 					phys.DestroyPhysical();
-
 				holding.transform.position = holdpoint.position;
 				holdpoint.localScale = Vector3.one;
 				transform.localScale = Vector3.one;
@@ -87,7 +76,7 @@ public class Inventory : Interactive, IExcludable {
 				holding.GetComponent<Rigidbody2D>().isKinematic = true;
 				holding.GetComponent<Collider2D>().isTrigger = true;
 				if (holding.pickupSounds.Length > 0)
-					GetComponent<AudioSource>().PlayOneShot(holding.pickupSounds[Random.Range(0,holding.pickupSounds.Length)]);
+					GetComponent<AudioSource>().PlayOneShot(holding.pickupSounds[Random.Range(0, holding.pickupSounds.Length)]);
 				UpdateActions();
 			}
 		}
@@ -111,23 +100,19 @@ public class Inventory : Interactive, IExcludable {
 	}
 
 	public void DropItem(){
-		
 		Messenger.Instance.DisclaimObject(holding.gameObject,this);
 		holding.GetComponent<Rigidbody2D>().isKinematic = false;
 		holding.GetComponent<Collider2D>().isTrigger = false;
-
 		PhysicalBootstrapper phys = holding.GetComponent<PhysicalBootstrapper>();
 		if (phys){
-			Vector2 initV = Vector2.ClampMagnitude( controllable.direction + Vector2.up,1f);
+			Vector2 initV = Vector2.ClampMagnitude(controllable.direction + Vector2.up, 1f);
 			initV = initV + GetComponent<Rigidbody2D>().velocity;
 			phys.InitPhysical(0.06f, initV);
 		} else {
 			holding.transform.parent = null;
 		}
-		
 		SpriteRenderer sprite = holding.GetComponent<SpriteRenderer>();
 		sprite.sortingLayerName = "main";
-
 		defaultInteraction = null;
 		holding = null;
 	}
@@ -138,23 +123,19 @@ public class Inventory : Interactive, IExcludable {
 			//update the possible manual actions.
 			manualActionDictionary = Interactor.ReportManualActions(holding.gameObject,gameObject);
 			// add inverse manual actions - is this proper?
-			List<Interaction> inverseActions = Interactor.ReportRightClickActions(gameObject,holding.gameObject);
+			List<Interaction> inverseActions = Interactor.ReportRightClickActions(gameObject, holding.gameObject);
 			foreach (Interaction inter in inverseActions)
 				if (!manualActionDictionary.Contains(inter))   // inverse double-count diode
 					manualActionDictionary.Add(inter);
-
 			defaultInteraction = Interactor.GetDefaultAction(manualActionDictionary);
 		}
-		UISystem.Instance.InventoryCallback(this,manualActionDictionary);
+		UISystem.Instance.InventoryCallback(this, manualActionDictionary);
 	}
 
 	public void RetrieveItem(string itemName){
-
 		for(int i=0; i < items.Count; i++){
-
 			//retrieve the item from the items
 			if(items[i].GetComponent<Item>().itemName == itemName){
-
 				if (holding)
 					DropItem();
 				items[i].SetActive(true);
@@ -162,10 +143,9 @@ public class Inventory : Interactive, IExcludable {
 				items[i].transform.parent = holdpoint;
 				items[i].GetComponent<Rigidbody2D>().isKinematic = true;
 				items[i].GetComponent<Collider2D>().isTrigger = true;
-				Messenger.Instance.ClaimObject(items[i].gameObject,this);
+				Messenger.Instance.ClaimObject(items[i].gameObject, this);
 				holding = items[i].GetComponent<Pickup>();
 				items.RemoveAt(i);
-				
 				UpdateActions();
 			}
 		}
@@ -189,7 +169,6 @@ public class Inventory : Interactive, IExcludable {
 				phys.InitPhysical(0.12f, Vector2.zero);
 				SpriteRenderer sprite = throwObject.GetComponent<SpriteRenderer>();
 				sprite.sortingLayerName = "main";
-
 				// these two commands have nothing to do with physical -- they're part of the drop code that sets physics
 				// back to normal.
 				throwObject.GetComponent<Rigidbody2D>().isKinematic = false;
@@ -220,28 +199,22 @@ public class Inventory : Interactive, IExcludable {
 		}
 		if(controllable.shootPressedFlag && defaultInteraction != null)
 			defaultInteraction.DoAction();
-
 		if(controllable.shootHeldFlag && defaultInteraction != null && defaultInteraction.continuous)
 			defaultInteraction.DoAction();
 	}
 
 	public void SwingItem(MeleeWeapon weapon){
-
 		swinging = true;
 		if (weapon.swingSounds.Length > 0){
-			GetComponent<AudioSource>().PlayOneShot(weapon.swingSounds[Random.Range(0,weapon.swingSounds.Length)]);
+			GetComponent<AudioSource>().PlayOneShot(weapon.swingSounds[Random.Range(0, weapon.swingSounds.Length)]);
 		}
-
 		slashFlag = "right";
-		
 		if( controllable.directionAngle > 45 && controllable.directionAngle < 135){
 			slashFlag = "up";
 		}
-		
 		if(controllable.directionAngle > 225 && controllable.directionAngle < 315){
 			slashFlag = "down";
 		}
-
 	}
 
 	void EndSwing(){
@@ -256,9 +229,7 @@ public class Inventory : Interactive, IExcludable {
 		slash.transform.localScale = Vector3.one;
 		holding.GetComponent<Renderer>().sortingLayerName="main";
 		holding.GetComponent<Renderer>().sortingOrder = GetComponent<Renderer>().sortingOrder + 1;
-
 		slash.GetComponent<Animator>().SetBool(slashFlag, true);
-
 		Physics2D.IgnoreCollision(holding.GetComponent<Collider2D>(), slash.GetComponent<Collider2D>(), true);
 		Slasher s = slash.GetComponent<Slasher>();
 		s.impactSounds = holding.GetComponent<MeleeWeapon>().impactSounds;
