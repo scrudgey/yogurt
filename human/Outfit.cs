b48946724248d8a0dbac6c3548aba399e18acbaa
RSHAW <scrudgey@gmail.com>
2015-10-31 13:47:05 -0700

    button change -- to be overhauled

--- human/Outfit.cs
+++ human/Outfit.cs
@@ -9,22 +9,19 @@ public class Outfit : Interactive {
 
 	void Start(){
 		advancedAnimation = GetComponent<AdvancedAnimation>();
-		
 		if (!LoadInitialized)
 			LoadInit();
-
 	}
 
 	public void LoadInit(){
-		interactions.Add(new Interaction(this,"Wear","DonUniform"));
-
+		Interaction wearInteraction = new Interaction(this, "Wear", "DonUniform");
+		wearInteraction.dontWipeInterface = false;
+		interactions.Add(wearInteraction);
 		LoadInitialized = true;
 	}
 
 	public void DonUniform(Uniform uniform){
-
 		RemoveUniform();
-
 		PhysicalBootstrapper phys = uniform.GetComponent<PhysicalBootstrapper>();
 		if (phys)
 			phys.DestroyPhysical();
@@ -34,10 +31,9 @@ public class Outfit : Interactive {
 	}
 
 	void RemoveUniform(){
-
 		// something has to change here if we're going to standardize name usage
-		GameObject uniform = Instantiate(Resources.Load("prefabs/"+wornUniformName) ) as GameObject;
-
+		string prefabName = Toolbox.Instance.ReplaceUnderscore(wornUniformName);
+		GameObject uniform = Instantiate(Resources.Load("prefabs/"+prefabName)) as GameObject;
 		uniform.transform.position = transform.position;
 		SpriteRenderer sprite = uniform.GetComponent<SpriteRenderer>();
 		sprite.sortingLayerName = "ground";
