04a427fd946bfb4359a6ae0bf256e20c67a67bdb
RSHAW <scrudgey@gmail.com>
2015-10-31 13:46:25 -0700

    changing button mode -- to be overhauled

--- human/Head.cs
+++ human/Head.cs
@@ -15,7 +15,11 @@ public class Head : Interactive, IExcludable {
 	}
 
 	void LoadInit(){
-		interactions.Add( new Interaction(this,"Wear","DonHat"));
+//		interactions.Add( new Interaction(this,"Wear","DonHat"));
+		Interaction wearAct = new Interaction(this, "Wear", "DonHat");
+		wearAct.dontWipeInterface = false;
+		interactions.Add(wearAct);
+
 		hatPoint = transform.Find("hatPoint").gameObject;
 		spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
 		intrinsics = gameObject.GetComponentInParent<Intrinsics>();
