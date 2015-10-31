8a80132079399a3149055baadc5f0f7861fe7eca
RSHAW <scrudgey@gmail.com>
2015-10-31 13:45:56 -0700

    interaction change -- to be overhauled

--- generics/Interactive.cs
+++ generics/Interactive.cs
@@ -19,7 +19,7 @@ public class Interaction {
 	public int defaultPriority;
 	public float range = 0.5f;
 	public bool limitless = false;
-	public bool dontWipeInterface = false;
+	public bool dontWipeInterface = true;
 	public string actionName;
 	public string displayVerb;
 
