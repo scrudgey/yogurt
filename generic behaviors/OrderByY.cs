7c0ee744a9997095265a2d941b881cbb4947ef5c
RSHAW <scrudgey@gmail.com>
2015-11-03 22:27:00 -0800

    initial commit

---
+++ generic behaviors/OrderByY.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OrderByY : MonoBehaviour {
+
+	SpriteRenderer spriteRenderer;
+	bool loadInit;
+
+	void Start () {
+		if (!loadInit)
+			LoadInit();
+	}
+
+	void LoadInit(){
+		loadInit = true;
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if (spriteRenderer.isVisible){
+			int pos = Mathf.RoundToInt(transform.position.y);
+			pos /= 3;
+//			spriteRenderer.sortingOrder = (pos * -1) + OrderOffset;
+//			spriteRenderer.sortingOrder = (pos * -1);
+			spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+		}
+	}
+}
