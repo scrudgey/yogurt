ef2c8c1599c8268f4c1425c5954e7105099a76b2
RSHAW <scrudgey@gmail.com>
2015-11-05 14:48:01 -0800

    first working version

--- generic behaviors/OrderByY.cs
+++ generic behaviors/OrderByY.cs
@@ -18,11 +18,20 @@ public class OrderByY : MonoBehaviour {
 
 	void Update () {
 		if (spriteRenderer.isVisible){
-			int pos = Mathf.RoundToInt(transform.position.y);
-			pos /= 3;
+//			int pos = Mathf.RoundToInt(transform.position.y);
+//			pos /= 3;
 //			spriteRenderer.sortingOrder = (pos * -1) + OrderOffset;
 //			spriteRenderer.sortingOrder = (pos * -1);
-			spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+//			spriteRenderer.sortingOrder = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+
+//			Vector3 tempPos = transform.position;
+//			tempPos.z = Mathf.RoundToInt(transform.position.y * 100f) * -1;
+//			tempPos.z = tempPos.y + 50f;
+//			transform.position = tempPos;
+			int pos = Mathf.RoundToInt(transform.position.y * 25f);
+//			pos /= 5; //Remember division of an INT and the modulus operator %? This isn't a float. We WANT to get rid of the remainder.
+//			spriteRenderer.sortingOrder = (pos * -1) + OrderOffset; 
+			spriteRenderer.sortingOrder = (pos * -1);
 		}
 	}
 }
