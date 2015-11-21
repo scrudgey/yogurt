69dc9b9870a18eee0e7a20d8d78f5e82bebdf64a
RSHAW <scrudgey@gmail.com>
2015-11-21 14:16:58 -0800

    fixed collision with player and put emit on fixedupdate loop

--- itemcode/FireExtinguisher.cs
+++ itemcode/FireExtinguisher.cs
@@ -12,21 +12,22 @@ public class FireExtinguisher : Interactive, IDirectable {
 	private Sprite defaultSprite;
 	private SpriteRenderer spriteRenderer;
 	private Vector2 direction = Vector2.right;
+	private bool doSpray;
 
 	void Start () {
-		Interaction spray = new Interaction(this,"Spray","Spray",false,true);
+		Interaction spray = new Interaction(this,"Spray", "Spray", false, true);
 		spray.defaultPriority = 1;
 		spray.continuous = true;
 		spray.dontWipeInterface = true;
 		interactions.Add(spray);
 
-		Interaction spray2 = new Interaction(this,"Spray","SprayObject",true,false);
+		Interaction spray2 = new Interaction(this, "Spray", "SprayObject", true, false);
 		spray2.continuous = true;
 		spray2.limitless = true;
 		spray2.dontWipeInterface = true;
 		spray2.displayVerb = "Spray at";
 		spray2.validationFunction = true;
-		interactions.Add (spray2);
+		interactions.Add(spray2);
 
 		if (!GetComponent<AudioSource>()){
 			gameObject.AddComponent<AudioSource>();
@@ -42,26 +43,36 @@ public class FireExtinguisher : Interactive, IDirectable {
 	
 	public void Spray(){
 		if (emissionTimeout <= 0f){
-			if(!GetComponent<AudioSource>().isPlaying ){
+			doSpray = true;
+		}
+	}
+
+	void FixedUpdate(){
+		if (doSpray){
+			if(!GetComponent<AudioSource>().isPlaying){
 				GetComponent<AudioSource>().clip = spraySound;
 				GetComponent<AudioSource>().Play();
 			}
-
 			spriteRenderer.sprite = spraySprite;
-			GameObject p = Instantiate(particle,transform.position,Quaternion.identity) as GameObject;
+			GameObject p = Instantiate(particle, transform.position, Quaternion.identity) as GameObject;
 			Vector2 force = direction * emissionSpeed;
-			Physics2D.IgnoreCollision(GetComponent<Collider2D>(),p.GetComponent<Collider2D>(),true);
-			Physics2D.IgnoreCollision(transform.parent.GetComponent<Collider2D>(),p.GetComponent<Collider2D>(),true);
-//			spriteRenderer.sortingLayerName="projectile";
-
-			p.GetComponent<Rigidbody2D>().AddForce(force * Time.deltaTime,ForceMode2D.Impulse);
+			
+			Collider2D projectileCollider = p.GetComponent<Collider2D>();
+//			Collider2D[] parentColliders = transform.parent.GetComponents<Collider2D>();
+			Collider2D[] parentColliders = gameObject.GetComponentsInParent<Collider2D>();
+			Physics2D.IgnoreCollision(GetComponent<Collider2D>(), projectileCollider, true);
+			foreach (Collider2D collider in parentColliders)
+				Physics2D.IgnoreCollision(collider, projectileCollider, true);
+			
+			p.GetComponent<Rigidbody2D>().AddForce(force * Time.deltaTime, ForceMode2D.Impulse);
 			emissionTimeout += emissionRate;
 		}
+		doSpray = false;
 	}
 
 	public void SprayObject(Item item){
 		if (emissionTimeout <= 0f){
-			direction = Vector3.ClampMagnitude(item.transform.position - transform.position ,1f);
+			direction = Vector3.ClampMagnitude(item.transform.position - transform.position, 1f);
 			Spray();
 		}
 	}
