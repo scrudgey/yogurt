86a602f483c2c9764ef2ee3262c6e577dbe0a2b2
RSHAW <scrudgey@gmail.com>
2015-11-23 01:06:02 -0800

    cleaned up old comments

--- singletons/Interactor.cs
+++ singletons/Interactor.cs
@@ -3,17 +3,7 @@
 using System.Collections.Generic;
 
 public class Interactor{
-
 	static public float interactionDistance = 0.4f;
-//	static string lastToolTip;
-//	static public List<Interaction> actionDictionary = new List<Interaction>();
-//	static private Vector3 mouseClickLocation;
-
-	//okay, why the fuck does removing this break everything?
-	// don't remove it!
-//	new void OnDestroy() {
-////		Debug.Log("something destroyed the interactor!!!");
-//	}
 
 	// maybe the most important method, it tells all available actions
 	// from the perspective of a player or whatever
@@ -112,57 +102,4 @@ public class Interactor{
 		return returnDictionary;
 
 	}
-
-	// n.b. : my own registered event- NOT OnMouseUp().
-//	public void MouseUp(){
-//		clickMenu = false;
-//		if (lastToolTip != null && lastToolTip != "")
-//			foreach (Interaction inter in actionDictionary)
-//				if (inter.actionName == lastToolTip)
-//					inter.DoAction();
-//		lastToolTip = null;
-//	}
-
-
-//	protected virtual void OnGUI(){
-//
-//		if ( externalVariableController){
-//			int items = actionDictionary.Count * 100;
-//			Vector3 coords = mouseClickLocation;
-//			coords.x -= 10;
-//			coords.y += 10;
-//
-//			GUILayout.BeginArea(new Rect(coords.x,Screen.height - coords.y,100,items));
-//			GUILayout.BeginVertical();
-//			foreach (Interaction action in actionDictionary)
-//				if( GUILayout.Button(new GUIContent(action.actionName,action.actionName),Toolbox.Instance.actionStyle) ) {}
-//
-//			GUILayout.EndVertical();
-//			GUILayout.EndArea();
-//
-//			if (Event.current.type == EventType.Repaint && GUI.tooltip != lastToolTip) 
-//				lastToolTip = GUI.tooltip;
-//				
-//		}
-//
-//		// this weird bit of fuckery avoids pissing off Unity's GUI handling system.
-//		// just don't touch it, okay?
-//		if (Event.current.type == EventType.Layout)
-//		{
-//			innerController = 1;
-//		}
-//		
-//		if (Event.current.type == EventType.Repaint)
-//		{
-//			innerController = 2;
-//		}
-//		
-//		if (innerController == 2)
-//		{
-//			externalVariableController = clickMenu && actionDictionary.Count > 0;
-//			innerController = 0;
-//		}
-//
-//	
-//	}
 }
