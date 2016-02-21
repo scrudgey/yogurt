20b03c19304d0ceb623b436900b482b164462295
RSHAW <scrudgey@gmail.com>
2016-02-21 14:54:20 -0800

    added new day functionality

--- UI/CommercialReportMenu.cs
+++ UI/CommercialReportMenu.cs
@@ -13,16 +13,20 @@ public class CommercialReportMenu : MonoBehaviour {
         SetRefs();
         // Time.timeScale = 0.0f;
     }
-    public void Done(){
-        // Time.timeScale = 1f;
+
+    public void ContinueButton(){
+        GameManager.Instance.activeCommercial = null;
         Destroy(gameObject);
+    }
+    public void NewDayButton(){
+        GameManager.Instance.NewDay();
     }
     
     private void SetRefs(){
         if (!titleText){
-            titleText = transform.Find("Image/TitleText").GetComponent<Text>();
-            descriptionText = transform.Find("Image/WhatText").GetComponent<Text>();
-            rewardText = transform.Find("Image/RewardText").GetComponent<Text>();
+            titleText = transform.Find("Image/titles/TitleText").GetComponent<Text>();
+            descriptionText = transform.Find("Image/desc/WhatText").GetComponent<Text>();
+            rewardText = transform.Find("Image/rew/RewardText").GetComponent<Text>();
         }
     }
     
