695eb1ebb355ece3cfa15c777f11a4a652e49350
RSHAW <scrudgey@gmail.com>
2016-02-26 12:46:49 -0800

    reacting to vomit

--- itemcode/VideoCamera.cs
+++ itemcode/VideoCamera.cs
@@ -8,6 +8,8 @@ public class VideoCamera : MonoBehaviour {
     public static Dictionary<string, string> KeyDescriptions = new Dictionary<string, string>{
         {"yogurt", "yogurts eaten"},
         {"vomit", "vomit events"},
+        {"yogurt_vomit", "yogurt emesis"},
+        {"yogurt_vomit_eat", "eating yogurt vomit"}
 	};
 	Text rec;
 	float blinkTimer;
@@ -40,34 +42,75 @@ public class VideoCamera : MonoBehaviour {
             interfaceTimeout -= Time.deltaTime;
         }
 	}
+        public void CalledCut(){
+        SaveCommercial();
+        GameManager.Instance.EvaluateCommercial(commercial);
+    }
+    void OnTriggerStay2D(Collider2D col){
+        if (col.gameObject == GameManager.Instance.playerObject){
+            interfaceTimeout = 0.5f;
+        }
+    }
+	void SaveCommercial(){
+		var serializer = new XmlSerializer(typeof(Commercial));
+			string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
+			path = Path.Combine(path, "commercial.xml");
+			// GameData data = new GameData(this);
+			FileStream sceneStream = File.Create(path);
+			serializer.Serialize(sceneStream, commercial);
+			sceneStream.Close();
+	}
 	
+    // TODO: there could be an issue here with the same occurrence triggering
+    // multiple collisions. I will have to handle that eventually.
 	void OnTriggerEnter2D(Collider2D col){        
 		if (col.name != "OccurrenceFlag(Clone)")
 		return;
 		Occurrence occurrence = col.gameObject.GetComponent<Occurrence>();
 		if (occurrence == null)
 		return;
-        ProcessOccurrence(occurrence, true);
+        ProcessOccurrence(occurrence);
+        // Dest
 	}
     
-    // Handle all the various occurrences
-    void ProcessOccurrence(Occurrence oc, bool debug=false){
-        if (debug){
-            Debug.Log(oc.functionName);
-            Debug.Log(oc.subjectName);
-            Debug.Log(oc.actor);
-            Debug.Log(oc.target);
-            foreach (System.Object parm in oc.parameters){
-                Debug.Log(parm);
+    void ProcessOccurrence(Occurrence oc){
+        foreach (OccurrenceData data in oc.data){
+            // Debug.Log(data.myType);
+            switch (data.myType){
+                case occurrenceType.eat:
+                    ProcessEat(data as OccurrenceEat);
+                    break;
+                case occurrenceType.vomit:
+                    ProcessVomit(data as OccurrenceVomit);
+                    break;
+                default:
+                    break;
             }
+            // handle qualities
+            commercial.data.AddData(data);
+            // trigger reaction to values increasing?
         }
-        
-        if (oc.subjectName.Contains("yogurt") && oc.functionName == "Drink"){
+    }
+    
+    void ProcessEat(OccurrenceEat data){
+        if (data.liquid.name == "Yogurt"){
             IncrementCommercialValue("yogurt", 1f);
-		}
+            if (data.liquid.vomit)
+                IncrementCommercialValue("yogurt_vomit_eat", 1f);
+        }
+    }
+    
+    void ProcessVomit(OccurrenceVomit data){
+        IncrementCommercialValue("vomit", 1f);
     }
     
     public void IncrementCommercialValue(string valname, float increment){
+        
+        CommercialProperty property = null;
+        commercial.properties.TryGetValue(valname, out property);
+        if (property == null){
+            commercial.properties[valname] = new CommercialProperty();
+        }
         
         float initvalue = commercial.properties[valname].val;
         float finalvalue = initvalue + increment;
@@ -80,26 +123,4 @@ public class VideoCamera : MonoBehaviour {
         
         commercial.properties[valname].val = finalvalue;
     }
-    
-    public void CalledCut(){
-        SaveCommercial();
-        GameManager.Instance.EvaluateCommercial(commercial);
-    }
-    
-    void OnTriggerStay2D(Collider2D col){
-        if (col.gameObject == GameManager.Instance.playerObject){
-            interfaceTimeout = 0.5f;
-        }
-    }
-	
-	void SaveCommercial(){
-		var serializer = new XmlSerializer(typeof(Commercial));
-			string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
-			path = Path.Combine(path, "commercial.xml");
-			// GameData data = new GameData(this);
-			FileStream sceneStream = File.Create(path);
-			serializer.Serialize(sceneStream, commercial);
-			sceneStream.Close();
-	}
-
 }
