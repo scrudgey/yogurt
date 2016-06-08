76fbe6afb6627b218f0366f9c477bbf3b804bc13
RSHAW <scrudgey@gmail.com>
2016-06-07 17:36:50 -0700

    major code cleanup and simplification

--- itemcode/Droplet.cs
+++ itemcode/Droplet.cs
@@ -2,28 +2,8 @@
 // using System.Collections;
 
 public class Droplet : MonoBehaviour {
-
-	// public Edible edible;
-	private MonoLiquid monoLiquid;
-	
-	// void Start () {
-	// 	monoLiquid = gameObject.AddComponent<MonoLiquid>();
-	// 	LiquidCollection.LoadMonoLiquid("Test Liquid 2",monoLiquid);
-	// 	SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
-	// 	if (spriteRenderer){
-	// 		spriteRenderer.color = monoLiquid.liquid.color;
-	// 	}
-	// }
-	
-	// public void LoadLiquid(string type){
-	// 	// make this fancier: retrieve liquid in buffer, only change variabel
-	// 	// if liquid was returned
-	// 	monoLiquid.liquid = LiquidCollection.LoadLiquid(type);
-		
-	// 	// add things here for changing edible properties according to liquid properties
-	// }
-	
 	void OnGroundImpact(Physical phys){
+		MonoLiquid monoLiquid = GetComponent<MonoLiquid>();
 		GameObject puddle = Instantiate(Resources.Load("Puddle"), transform.position, Quaternion.identity) as GameObject;
 		puddle.layer = 8;
 		PhysicalBootstrapper pb = GetComponent<PhysicalBootstrapper>();
