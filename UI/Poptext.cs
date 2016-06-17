1d8594c39c1f8bcf76c125b7a8607c49f7c6287e
RSHAW <scrudgey@gmail.com>
2016-06-16 19:29:49 -0700

    asynchronous multiple popups

--- UI/Poptext.cs
+++ UI/Poptext.cs
@@ -1,35 +1,37 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Poptext : MonoBehaviour {
-    public string description = "test";
+    public List<string> description = new List<string>();
     private HueShiftText hueShifter;
-    public float initValue;
-    public float finalValue;
+    public List<float> initValueList = new List<float>();
+    public List<float> finalValueList = new List<float>();
+
     private Text descriptionText;
     private Text valueText;
     private float lifetime;
     private GameObject dock;
-    
     public bool colorFX = false;
-    
     private AudioSource audioSource;
     public AudioClip incrementSound;
-	void Start () {
-	   descriptionText = transform.Find("dock/Text").GetComponent<Text>();
-       valueText = transform.Find("dock/value").GetComponent<Text>();
-       hueShifter = transform.Find("dock/value").GetComponent<HueShiftText>();
-       dock = transform.Find("dock").gameObject;
-       audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
+    public VideoCamera video;
+	public void Start () {
+        descriptionText = transform.Find("dock/Text").GetComponent<Text>();
+        valueText = transform.Find("dock/value").GetComponent<Text>();
+        hueShifter = transform.Find("dock/value").GetComponent<HueShiftText>();
+        dock = transform.Find("dock").gameObject;
+        audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
+        audioSource.volume = 0.25f;
+        hueShifter.enabled = false;
+        hueShifter.speedConst = 0.3f;
+
+        if (description.Count > 0){
+            descriptionText.text = description[0] + ":";
+            valueText.text = initValueList[0].ToString();
+        }
        
-       hueShifter.enabled = false;
-       hueShifter.speedConst = 0.3f;
-       
-       if (descriptionText.text != description){
-           descriptionText.text = description + ":";
-       }
-       valueText.text = initValue.ToString();
        
        StartCoroutine(Display());
 	}
@@ -51,14 +53,29 @@ public class Poptext : MonoBehaviour {
         }
         yield return new WaitForSeconds(0.25f);
         
-        valueText.text = finalValue.ToString();
-        audioSource.PlayOneShot(incrementSound);
-        if (colorFX){
-            valueText.color = Color.red;
-            hueShifter.enabled = true;
+        while (finalValueList.Count > 0){
+            descriptionText.text = description[0] + ":";
+            description.Remove(description[0]);
+
+            float finalValue = finalValueList[0];
+            finalValueList.RemoveAt(0);
+            if (description.Count > 0){
+                hangtime = 1.2f;
+            }
+
+            valueText.text = finalValue.ToString();
+            audioSource.PlayOneShot(incrementSound);
+            if (colorFX){
+                valueText.color = Color.red;
+                hueShifter.enabled = true;
+            }
+            if (video){
+                // UI check if commercial is complete
+                video.CheckForFinishState();
+            }
+            
+            yield return new WaitForSeconds(hangtime);
         }
-        
-        yield return new WaitForSeconds(hangtime);
         
         hueShifter.enabled = false;
         valueText.color = Color.white;
@@ -72,6 +89,7 @@ public class Poptext : MonoBehaviour {
             yield return null;
         } 
         
+        Destroy(gameObject);
         yield return null;
     }
     float easing(float t, float b, float c, float d){
