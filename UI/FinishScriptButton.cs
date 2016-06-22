9ce0aaaced7acff371276672f621fe9e1c23c619
RSHAW <scrudgey@gmail.com>
2016-06-22 15:26:05 -0700

    new UI system for finishing script

---
+++ UI/FinishScriptButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class FinishScriptButton : MonoBehaviour {
+	public void Clicked(){
+		VideoCamera video = GameObject.FindObjectOfType<VideoCamera>();
+		video.live = false;
+		if (video){
+			GameManager.Instance.EvaluateCommercial(video.commercial);
+		}
+		// GameManager.Instance.EvaluateCommercial();
+	}
+}
