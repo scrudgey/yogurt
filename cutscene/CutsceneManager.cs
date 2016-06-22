a90d31ce05d5c0909315644c8921ec0e620b0c6d
RSHAW <scrudgey@gmail.com>
2016-06-22 15:26:41 -0700

    initial commit of cutscene code

---
+++ cutscene/CutsceneManager.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using Easings;
+
+public class CutsceneManager : MonoBehaviour {
+
+    public enum CutsceneType {newDay}
+    public CutsceneType type = CutsceneType.newDay;
+    private float timer;
+    Text tomText;
+    Text dayText;
+    GameObject canvas;
+
+    private float stopTomFade = 1.5f;
+    private float startDayFade = 1.5f;
+    private float stopDayFade = 2.5f;
+    private float stopTime = 12f;
+
+    void Start(){
+        if (type == CutsceneType.newDay){
+            canvas = GameObject.Find("Canvas");
+            tomText = canvas.transform.Find("tomText").GetComponent<Text>();
+            dayText = canvas.transform.Find("dayText").GetComponent<Text>();
+
+            Color blank = new Color(255, 255, 255, 0);
+            tomText.color = blank;
+            dayText.color = blank;
+        }
+    }
+
+    void Update(){
+        timer += Time.deltaTime;
+        if (timer < stopTomFade){
+            Color col = tomText.color;
+            col.a = (float)PennerDoubleAnimation.ExpoEaseIn(timer, 0, 1, stopTomFade);
+            tomText.color = col;
+        }
+        if (timer > startDayFade && timer < stopDayFade){
+            Color col = dayText.color;
+            col.a = (float)PennerDoubleAnimation.ExpoEaseIn(timer - startDayFade, 0, 1, stopDayFade - startDayFade);
+            dayText.color = col;
+        }
+        if (timer >= stopTime || Input.anyKey){
+            GameManager.Instance.NewDay();
+        }
+    }
+    
+}
---
+++ cutscene/CutsceneObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CutsceneObject : MonoBehaviour {
+
+	public float dx;
+	public float dy;
+	public float tickInterval;
+	private float timer;
+
+	void Start(){
+		timer = 0f;
+	}
+	void Update(){
+		timer += Time.deltaTime;
+		if (timer > tickInterval){
+			Vector3 tempPos = transform.position;
+			tempPos.x += dx;
+			tempPos.y += dy;
+			transform.position = tempPos;
+			timer = 0f;
+		}
+	}
+}
