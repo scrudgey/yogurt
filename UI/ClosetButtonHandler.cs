2458503dbae4e86da6ea599cbd6f46b9aecf180c
RSHAW <scrudgey@gmail.com>
2016-06-29 17:26:09 -0700

    generalized closet to fridge, dresser

--- UI/ClosetButtonHandler.cs
+++ UI/ClosetButtonHandler.cs
@@ -1,15 +1,18 @@
 ﻿using UnityEngine;
-// using System.Collections;
+using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class ClosetButtonHandler : MonoBehaviour {
 	private Image icon;
+	private Text titleText;
 	void Start(){
-		GameObject iconObject = transform.Find("menu/body/InfoPanel/ImagePanel/Icon").gameObject;
-		icon = iconObject.GetComponent<Image>();
+		// GameObject iconObject = transform.Find("menu/body/InfoPanel/ImagePanel/Icon").gameObject;
+		// icon = iconObject.GetComponent<Image>();
+		icon = transform.Find("menu/body/InfoPanel/ImagePanel/Icon").GetComponent<Image>();
+		// titleText = transform.Find("menu/menubar/titlebar").GetComponent<Text>();
 		icon.sprite = null;
 		icon.color = new Color(1f, 1f, 1f, 0f);
-		PopulateItemList();
+		// PopulateItemList();
 	}
 	
 	private GameObject spawnEntry(){
@@ -17,9 +20,22 @@ public class ClosetButtonHandler : MonoBehaviour {
 		return newObject;
 	}
 	
-	private void PopulateItemList(){
+	public void PopulateItemList(HomeCloset.ClosetType type){
 		GameObject listObject = transform.Find("menu/body/ItemList").gameObject;
-		foreach (string name in GameManager.Instance.data.collectedItems){
+		List<string> itemList = GameManager.Instance.data.collectedObjects;
+		titleText = transform.Find("menu/menubar/titlebar").GetComponent<Text>();
+		if (type == HomeCloset.ClosetType.clothing){
+			itemList = GameManager.Instance.data.collectedClothes;
+			titleText.text = "Collected Clothing";
+		} else if (type == HomeCloset.ClosetType.food){
+			itemList = GameManager.Instance.data.collectedFood;
+			titleText.text = "Collected Food";
+		} else if (type == HomeCloset.ClosetType.items){
+			itemList = GameManager.Instance.data.collectedItems;
+			titleText.text = "Collected Items";
+		}
+		// foreach (string name in GameManager.Instance.data.collectedItems){
+		foreach (string name in itemList){
 			GameObject newEntry = spawnEntry();
 			ItemEntryScript entryScript = newEntry.GetComponent<ItemEntryScript>();
 			entryScript.itemName = name;
@@ -32,9 +48,6 @@ public class ClosetButtonHandler : MonoBehaviour {
 	public void CloseButtonClick(){
 		UINew.Instance.CloseClosetMenu();
 	}
-	// public void GetButtonClick(){
-		
-	// }
 	
 	public void ItemClick(ItemEntryScript itemScript){
 		GameManager.Instance.RetrieveCollectedItem(itemScript.itemName);
