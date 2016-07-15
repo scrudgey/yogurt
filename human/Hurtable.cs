eb16658b42b37d8a0114c79224f3952e8801a69d
RSHAW <scrudgey@gmail.com>
2016-07-15 12:44:02 -0700

    adding unconsciousness

--- human/Hurtable.cs
+++ human/Hurtable.cs
@@ -12,6 +12,8 @@ public class Hurtable : MonoBehaviour, IMessagable {
 	private bool hitstun;
 	private bool doubledOver;
 	public float impulse;
+	private bool knockDown;
+	private float downedTimer;
 
 	public void TakeDamage(damageType type, float amount){
 		if (!myIntrinsic.invulnerable.boolValue){
@@ -20,6 +22,7 @@ public class Hurtable : MonoBehaviour, IMessagable {
 				if (!myIntrinsic.no_physical_damage.boolValue){
 					health -= amount;
 					impulse += amount;
+					
 				}
 				break;
 			case damageType.fire:
@@ -31,12 +34,7 @@ public class Hurtable : MonoBehaviour, IMessagable {
 				break;
 			}
 		}
-		if (health <= 0){
-			health = 0f;
-			if (!unconscious){
-				unconscious = true;
-			}
-		}
+
 		if (!hitstun){
 			hitstun = true;
 			hitStunCounter = 0.25f;
@@ -62,31 +60,90 @@ public class Hurtable : MonoBehaviour, IMessagable {
 			TakeDamage(dam.type, dam.amount);
 			if (dam.impactor)	
 				dam.impactor.PlayImpactSound();
+			Rigidbody2D body = GetComponent<Rigidbody2D>();
+			if (body){
+				Debug.Log("adding force "+dam.force.ToString());
+				body.AddForce(dam.force * 100f);
+			}
 		}
 	}
 
 	public void Update(){
+		if (impulse > 0){
+			impulse -= Time.deltaTime * 25f;
+		}
+		if (downedTimer > 0){
+			downedTimer -= Time.deltaTime;
+		}
 		if (hitStunCounter > 0){
 			hitStunCounter -= Time.deltaTime;
-			if (hitStunCounter <= 0 && !doubledOver){
+			if (hitStunCounter <= 0 && !doubledOver && !knockDown){
 				hitstun = false;
-
 				MessageHitstun message = new MessageHitstun();
 				message.value = false;
 				Toolbox.Instance.SendMessage(gameObject, this, message);
 			}
 		}
-		if (impulse > 0){
-			impulse -= Time.deltaTime * 25f;
+		if (health < 0.5 * maxHealth){
+			health += Time.deltaTime;
 		}
-		if (impulse > 30f && !doubledOver){
+		if (health <= 0 && !unconscious){
+			unconscious = true;
+			if (!knockDown)
+				KnockDown();
+		}
+
+		if (unconscious && health > 0){
+			unconscious = false;
+		}
+
+
+		if (impulse > 50f && !knockDown){
+			KnockDown();
+		}
+		if (downedTimer <= 0 && knockDown && health > 0){
+			GetUp();
+		}
+		if (impulse > 35f && !doubledOver && !knockDown){
+			DoubleOver(true);
+		}
+		if (impulse <= 0f && doubledOver &&!knockDown){
+			DoubleOver(false);
+		}
+	}
+
+	public void KnockDown(){
+		knockDown = true;
+		doubledOver = false;
+		downedTimer = 2f;
+		Vector3 pivot = transform.position;
+		pivot.y -= 0.15f;
+		transform.RotateAround(pivot, new Vector3(0, 0, 1), -90);
+		MessageHitstun message = new MessageHitstun();
+		message.value = true;
+		message.doubledOver = false;
+		Toolbox.Instance.SendMessage(gameObject, this, message);
+	}
+
+	public void GetUp(){
+		knockDown = false;
+		hitstun = false;
+		doubledOver = false;
+		Vector3 pivot = transform.position;
+		pivot.x -= 0.15f;
+		transform.RotateAround(pivot, new Vector3(0, 0, 1), 90);
+		MessageHitstun message = new MessageHitstun();
+		message.value = false;
+		Toolbox.Instance.SendMessage(gameObject, this, message);
+	}
+	public void DoubleOver(bool val){
+		if (val){
 			doubledOver = true;
 			MessageHitstun message = new MessageHitstun();
 			message.value = true;
 			message.doubledOver = true;
 			Toolbox.Instance.SendMessage(gameObject, this, message);
-		}
-		if (impulse <= 0f && doubledOver){
+		} else {
 			doubledOver = false;
 			hitstun = false;
 			MessageHitstun message = new MessageHitstun();
@@ -95,5 +152,6 @@ public class Hurtable : MonoBehaviour, IMessagable {
 			Toolbox.Instance.SendMessage(gameObject, this, message);
 		}
 	}
+	
 
 }
