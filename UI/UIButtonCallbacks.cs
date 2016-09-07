b6ca676f9fd296f05a0e14dd8d61d64c663e1c7f
RSHAW <scrudgey@gmail.com>
2016-09-06 23:33:30 -0700

    callback codes for UI buttons fight, inventory etc

---
+++ UI/UIButtonCallbacks.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class UIButtonCallbacks : MonoBehaviour {
+	public void FightButtonClick(){
+		GameManager.Instance.ToggleFightMode();
+	}
+	public void SpeakButtonClick(){
+        GameObject test = GameObject.Find("DialogueMenu(Clone)");
+        if (!test){
+            GameObject obj = Instantiate(Resources.Load("UI/DialogueMenu")) as GameObject;
+            obj.GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
+        }
+    }
+	public void InventoryButtonClick(){
+		UINew.Instance.inventoryVisible = !UINew.Instance.inventoryVisible;
+		if (UINew.Instance.inventoryVisible){
+			UINew.Instance.ShowInventoryMenu();
+		} else {
+			UINew.Instance.CloseInventoryMenu();
+		}
+	}
+}
