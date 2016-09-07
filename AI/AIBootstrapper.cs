ef30418a48b2ef473df4db648b1f5e377732b86d
RSHAW <scrudgey@gmail.com>
2016-09-06 23:32:51 -0700

    start of AI v2 with a bootstrapper component to initialize and set up all the component pieces

---
+++ AI/AIBootstrapper.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+// using System.Collections;
+
+public class AIBootstrapper : MonoBehaviour {
+	public Awareness awareness;
+	public EntityController controller;
+
+	public enum InitialState{stand, wander}
+	public InitialState initialState; 
+
+	void Start(){
+		awareness = Toolbox.Instance.GetOrCreateComponent<Awareness>(gameObject);
+		controller = Toolbox.Instance.GetOrCreateComponent<EntityController>(gameObject);
+
+		awareness.controller = controller;
+		controller.bootstrapper = this;
+		InitializeController();
+	}
+
+	public void InitializeController(){
+		if (controller.priority == null)
+			controller.priority = new Priority();
+			
+		switch(initialState){
+			case InitialState.stand:
+			break;
+			case InitialState.wander:
+			controller.priority.Wander();
+			break;
+			default:
+			break;
+		}
+	}
+}
