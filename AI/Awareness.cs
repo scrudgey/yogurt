649d01a3d4d4eac048eb5396c1e9a16c68fcda26
RSHAW <scrudgey@gmail.com>
2016-09-12 21:14:14 -0700

    fully works with setting and updating personal assessments upon being punched

--- AI/Awareness.cs
+++ AI/Awareness.cs
@@ -14,7 +14,7 @@ public class Knowledge{
 	public GameObject obj;
 	public Quality quality;
 	public Vector3 lastSeenPosition;
-
+	public Vector3 lastSeenTime;
 	public Knowledge(GameObject o){
 		obj = o;
 		Qualities q = o.GetComponent<Qualities>();
@@ -25,22 +25,22 @@ public class Knowledge{
 }
 
 public class PersonalAssessment{
-	public enum tag{neutral, friend, enemy}
-	public tag friendStatus;
-	public float feelingToward;
+	public enum friendStatus{neutral, friend, enemy}
+	public friendStatus status;
 	public float harmDealt;
 }
 
-public class Awareness : MonoBehaviour {
+public class Awareness : MonoBehaviour, IMessagable {
 	private GameObject sightCone;
 	private Vector3 sightConeScale;
 	private Controllable control;
 	private float speciousPresent; 
 	private List<GameObject> fieldOfView = new List<GameObject>();
 	private bool viewed;
-	public Dictionary<GameObject,Knowledge> objects = new Dictionary<GameObject,Knowledge>();
-	public Dictionary<GameObject,PersonalAssessment> people = new Dictionary<GameObject, PersonalAssessment>();
+	public Dictionary<GameObject, Knowledge> objects = new Dictionary<GameObject, Knowledge>();
+	public Dictionary<GameObject, PersonalAssessment> people = new Dictionary<GameObject, PersonalAssessment>();
 	public EntityController controller;
+	public DecisionMaker decisionMaker;
 
 	void Start () {
 		control = gameObject.GetComponent<Controllable>();
@@ -106,8 +106,15 @@ public class Awareness : MonoBehaviour {
 				fieldOfView = new List<GameObject>();
 				viewed = true;
 			}
+			if (fieldOfView.Contains(gameObject))
+				return;
+			
 			if (other.tag == "Physical")
 				fieldOfView.Add(other.gameObject);
+
+			if (other.gameObject.GetComponent<Controllable>()){
+				fieldOfView.Add(other.gameObject);
+			}
 		}
 	}
 	// process the list of objects in the field of view.
@@ -115,11 +122,19 @@ public class Awareness : MonoBehaviour {
 		viewed = false;
 		speciousPresent = 1f;
 		foreach (GameObject g in fieldOfView){
-			Qualities q = Toolbox.Instance.GetQuality(g);
+			// Qualities q = Toolbox.Instance.GetQuality(g);
+			Qualities q = Toolbox.Instance.GetOrCreateComponent<Qualities>(g);
 			Quality quality = q.quality;
 			if (quality.flaming && controller.priority.name != "extinguish"){
 				controller.priority.ExtinguishObject(g);
 				controller.CheckPriority();
+			}
+			PersonalAssessment assessment = FormPersonalAssessment(g);
+			if (assessment != null){
+				// check to see if person is enemy
+				if (assessment.status == PersonalAssessment.friendStatus.enemy){
+					Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("You are mine enemy!"));
+				}
 			}
 		}
 	}
@@ -129,7 +144,6 @@ public class Awareness : MonoBehaviour {
 			Knowledge newKnowledge = new Knowledge(col.gameObject);
 			objects.Add(col.gameObject,newKnowledge);
 			React(col.gameObject);
-
 		}
 		if (keys.Contains(col.gameObject)){
 			objects[col.gameObject].lastSeenPosition = col.gameObject.transform.position;
@@ -137,7 +151,39 @@ public class Awareness : MonoBehaviour {
 	}
 	void React(GameObject g){
 		if (g.name == "pimp_hat"){
-			SendMessage("Say","That's a sweet hat!",SendMessageOptions.DontRequireReceiver);
+			// SendMessage("Say", "That's a sweet hat!", SendMessageOptions.DontRequireReceiver);
+			Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("Sweet hat!"));
+		}
+	}
+
+	PersonalAssessment FormPersonalAssessment(GameObject g){
+		if (!g.GetComponent<Controllable>()){
+			Debug.Log(g.name+" is not controllable");
+			return null;
+		}
+
+		PersonalAssessment storedAssessment;
+		if (people.TryGetValue(g, out storedAssessment)){
+			return storedAssessment;
+		}
+		PersonalAssessment assessment = new PersonalAssessment();
+		people.Add(g, assessment);
+		return assessment;
+	}
+
+	void AttackedByPerson(GameObject g){
+		PersonalAssessment assessment = FormPersonalAssessment(g);
+		if (assessment != null){
+			assessment.status = PersonalAssessment.friendStatus.enemy;
+		}
+	}
+
+	public void ReceiveMessage(Message incoming){
+		if (incoming is MessageDamage){
+			MessageDamage message = (MessageDamage)incoming;
+			foreach (GameObject responsible in message.responsibleParty){
+				AttackedByPerson(responsible);
+			}
 		}
 	}
 }
