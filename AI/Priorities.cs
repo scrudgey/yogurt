54da180b20e626f5381c3cb032b07237f5665a19
RSHAW <scrudgey@gmail.com>
2016-09-30 15:06:26 -0700

    changed Goalfactory to a series of goal subclasses

--- AI/Priorities.cs
+++ AI/Priorities.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 namespace AI{
 	[System.Serializable]
@@ -14,30 +15,23 @@ namespace AI{
 			gameObject = g;
 			control = c;
 		}
-		public virtual void Update () {
-
-		}
-		public virtual void GoalFinished(status goalStatus){
-
-		}
+		public virtual void Update(){}
 		public void DoAct(){
-			// the code to actually enact the current goal
 			if (goal != null){
 				status goalStatus = goal.Update();
 				if (goalStatus != status.neutral)
 					GoalFinished(goalStatus);
 			}
 		}
-		public virtual void ReceiveMessage(Message m){
-
-		}
+		public virtual void ReceiveMessage(Message m){}
+		public virtual void GoalFinished(status goalStatus){}
 	}
 
 	public class PriorityFightFire: Priority{
 		public PriorityFightFire(GameObject g, Controllable c): base(g, c) {
-			goal = GoalFactory.GetItemGoal(g, c, "fire extinguisher");
+			// goal = GoalFactory.GetItemGoal(g, c, "fire extinguisher");
+			goal = new GoalGetItem(g, c, "fire extinguisher");
 		}
-
 		// get a fire extinguisher if I don't have one
 		// walk to and hose down whatever flaming object I know about
 		// if I don't know of any flaming objects, search for one
@@ -47,13 +41,13 @@ namespace AI{
 		public override void Update(){
 		}
 		public override void GoalFinished(status goalStatus){
-
 		}
 	}
 
 	public class PriorityWander: Priority{
 		public PriorityWander(GameObject g, Controllable c): base(g, c) {
-			goal = GoalFactory.WanderGoal(g, c);
+			// goal = GoalFactory.WanderGoal(g, c);
+			goal = new GoalWander(g, c);
 			goal.Init(gameObject, control);
 		}
 		public override void Update(){
@@ -64,17 +58,51 @@ namespace AI{
 	public class PriorityRunAway: Priority{
 		GameObject threat;
 		public PriorityRunAway(GameObject g, Controllable c): base(g, c){
-			goal = GoalFactory.WanderGoal(g, c);
+			// goal = GoalFactory.WanderGoal(g, c);
+			goal = new GoalWander(g, c);
 		}
 		public override void ReceiveMessage(Message incoming){
 			if (incoming is MessageDamage){
 				MessageDamage dam = (MessageDamage)incoming;
 				urgency += 1;
 				threat = dam.responsibleParty[0];
-				goal = GoalFactory.RunFromObject(gameObject, control, threat);
+				// goal = GoalFactory.RunFromObject(gameObject, control, threat);
+				goal = new GoalRunFromObject(gameObject, control, threat);
 			}
 		}
-
 	}
 
+	public class PriorityAttack: Priority{
+		public HashSet<GameObject> enemies = new HashSet<GameObject>();
+		public GameObject closestEnemy;
+		private float updateInterval;
+		public PriorityAttack(GameObject g, Controllable c): base(g, c){
+			goal = new GoalWander(g, c);
+		}
+		public override void ReceiveMessage(Message incoming){
+			if (incoming is MessageDamage){
+				MessageDamage dam = (MessageDamage)incoming;
+				urgency += 1;
+				enemies.Add(dam.responsibleParty[0]);
+			}
+		}
+		public override void Update(){
+			if (updateInterval > 0){
+				updateInterval -= Time.deltaTime;
+				return;
+			}
+			updateInterval = 0.5f;
+			float closestDist = 999f;
+			if (closestEnemy != null){
+				closestDist = Vector2.Distance(gameObject.transform.position, closestEnemy.transform.position);
+			}
+			foreach (GameObject enemy in enemies){
+				float dist = Vector2.Distance(gameObject.transform.position, enemy.transform.position);
+				if (closestDist > dist){
+					closestEnemy = enemy;
+					closestDist = dist;
+				}
+			}
+		}
+	}
 }
