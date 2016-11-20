e8d666ea7c7505de8bef7bc506919fe4a579353c
RSHAW <scrudgey@gmail.com>
2016-11-19 21:41:46 -0800

    required code for something like a simple bubble animation

---
+++ generic behaviors/AnimateFrames.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class AnimateFrames : MonoBehaviour {
+	public List<Sprite> frames;
+	private SpriteRenderer spriteRenderer;
+	private float animationTimer;
+	public float frameTime = 1f;
+	private int frameIndex = 0;
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		if (spriteRenderer == null){
+			Destroy(this);
+		}
+		spriteRenderer.sprite = frames[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		animationTimer += Time.deltaTime;
+		if (animationTimer > frameTime){
+			frameIndex += 1;
+			if (frameIndex == frames.Count){
+				frameIndex = 0;
+			}
+			animationTimer = 0f;
+			spriteRenderer.sprite = frames[frameIndex];
+		}
+	}
+}
