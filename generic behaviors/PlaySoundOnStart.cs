665dfc9fa9827732070e45e7e308d7d40ac06f97
RSHAW <scrudgey@gmail.com>
2016-11-25 18:07:27 -0800

    initial commit

---
+++ generic behaviors/PlaySoundOnStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+public class PlaySoundOnStart : MonoBehaviour {
+	public AudioClip sound;
+	private AudioSource source;
+	void Start () {
+		source = Toolbox.Instance.SetUpAudioSource(gameObject);
+		if (sound != null)
+			source.PlayOneShot(sound);
+	}
+}
