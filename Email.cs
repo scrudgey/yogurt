3542974004f3e4af86ddfdd3b4a4df3f251f0f7d
RSHAW <scrudgey@gmail.com>
2016-11-26 14:28:20 -0800

    initial commit of email system

---
+++ Email.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Xml.Serialization;
+using System.IO;
+
+[System.Serializable]
+public class Email {
+	public string fromString;
+	public string toString;
+	public string subject;
+	public string content;
+	public bool read;
+
+	public static Email LoadEmail(string filename){
+		try{
+            Email newEmail = null;
+            TextAsset xml = Resources.Load("data/emails/"+filename) as TextAsset;
+            var serializer = new XmlSerializer(typeof(Email));
+            var reader = new System.IO.StringReader(xml.text);
+            newEmail = serializer.Deserialize(reader) as Email;
+            return newEmail;
+        } catch(Exception e){
+            Debug.Log(e.Message);
+            return null;
+        }
+	}
+
+	public static void SaveEmail(Email email){
+		var serializer = new XmlSerializer(typeof(Email));
+		string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
+		path = Path.Combine(path, "email.xml");
+		FileStream sceneStream = File.Create(path);
+		serializer.Serialize(sceneStream, email);
+		sceneStream.Close();
+	}
+}
---
+++ UI/EmailUI.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class EmailUI : MonoBehaviour {
+	public GameObject emailListObject;
+	public Text emailText;
+	public Text toText;
+	public Text fromText;
+	public Text subjectText;
+	public List<emailEntryButton> emailButtons;
+	public Computer computer;
+	// private Canvas myCanvas;
+	void Start () {
+		GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
+
+		emailButtons = new List<emailEntryButton>();
+
+		emailListObject = transform.Find("main/window/listPanel/list/Viewport/Content").gameObject;
+		toText = transform.Find("main/window/emailView/info/to").GetComponent<Text>();
+		fromText = transform.Find("main/window/emailView/info/from").GetComponent<Text>();
+		subjectText = transform.Find("main/window/emailView/info/subject").GetComponent<Text>();
+		emailText = transform.Find("main/window/emailView/text/content").GetComponent<Text>();
+
+		InitializeEmailList();
+	}
+	public void InitializeEmailList(){
+		// remove any existing entries
+		// retrieve an email list, populate the list of buttons
+		Destroy(emailListObject.transform.GetChild(0).gameObject);
+
+		foreach (Email email in GameManager.Instance.data.emails){
+			GameObject entry = GameObject.Instantiate(Resources.Load("UI/emailEntry")) as GameObject;
+			emailEntryButton entryScript = entry.GetComponent<emailEntryButton>();
+			entryScript.Initialize(this, email);
+			entry.transform.SetParent(emailListObject.transform, false);
+			emailButtons.Add(entryScript);
+			// entry.GetComponent<RectTransform>().localScale = Vector3.one;
+		}
+	}
+	public void EmailEntryCallback(Email email){
+		// populate the text entries with the information from the email
+		emailText.text = email.content;
+		toText.text = "To: "+email.toString;
+		fromText.text = "From: "+email.fromString;
+		subjectText.text = "Subject: "+email.subject;
+		email.read = true;
+
+		foreach (emailEntryButton button in emailButtons){
+			button.CheckReadStatus();
+		}
+		computer.CheckBubble();
+	}
+
+	public void OKButtonCallback(){
+		Destroy(gameObject);
+		computer.CheckBubble();
+	}
+}
