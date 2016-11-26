e82c14c6c9f73c06348253015284e5f2aee5b320
RSHAW <scrudgey@gmail.com>
2016-11-26 14:29:00 -0800

    fixed strange indentation

--- itemcode/VideoCamera.cs
+++ itemcode/VideoCamera.cs
@@ -19,11 +19,11 @@ public class VideoCamera : MonoBehaviour, IMessagable {
 	}
 	void SaveCommercial(){
 		var serializer = new XmlSerializer(typeof(Commercial));
-			string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
-			path = Path.Combine(path, "commercial.xml");
-			FileStream sceneStream = File.Create(path);
-			serializer.Serialize(sceneStream, commercial);
-			sceneStream.Close();
+        string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
+        path = Path.Combine(path, "commercial.xml");
+        FileStream sceneStream = File.Create(path);
+        serializer.Serialize(sceneStream, commercial);
+        sceneStream.Close();
 	}
     // TODO: there could be an issue here with the same occurrence triggering
     // multiple collisions. I will have to handle that eventually.
