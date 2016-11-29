1cf55df01806eea311e77dd8dff00988da22a146
RSHAW <scrudgey@gmail.com>
2016-11-29 11:49:46 -0800

    initial commit

---
+++ UI/emailEntryButton.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class emailEntryButton : MonoBehaviour {
+	private EmailUI emailUI;
+	private Email email;
+	public Text newText;
+	public Text nameText;
+
+	public void Initialize(EmailUI ui, Email initEmail){
+		emailUI = ui;
+		email = initEmail;
+		newText = transform.Find("new").GetComponent<Text>();
+		nameText = transform.Find("name").GetComponent<Text>();
+
+		nameText.text = email.subject;
+		CheckReadStatus();
+	}
+
+	public void CheckReadStatus(){
+		if (email.read){
+			newText.text = "";
+		} else {
+			newText.text = "!";
+		}
+	}
+	public void Clicked(){
+		emailUI.EmailEntryCallback(email);
+		email.read = true;
+	}
+}
---
+++ specialObjects/Computer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+public class Computer : Item {
+	GameObject newBubble;
+	void Start () {
+		interactions.Add(new Interaction(this, "Email", "OpenEmail"));
+		newBubble = transform.Find("newBubble").gameObject;
+		CheckBubble();
+	}
+	public void CheckBubble(){
+		bool activeBubble = false;
+		if (GameManager.Instance.data == null)
+			return;
+		foreach (Email email in GameManager.Instance.data.emails){
+			if (email.read == false)
+				activeBubble = true;
+		}
+		newBubble.SetActive(activeBubble);
+	}
+	public void OpenEmail(){
+		GameObject menu = GameObject.Instantiate(Resources.Load("UI/EmailUI")) as GameObject;
+		menu.GetComponent<EmailUI>().computer = this;
+	}
+}
