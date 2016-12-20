1b5395f08a1a5a24759bf921cc91a534fc427a14
RSHAW <scrudgey@gmail.com>
2016-12-19 21:30:18 -0800

    variable diary text

--- UI/Diary.cs
+++ UI/Diary.cs
@@ -1,9 +1,17 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Diary : MonoBehaviour {
+	public Text diaryText;
+	public string loadDiaryName;
 	public void Start(){
+		diaryText = transform.Find("diaryPanel/diaryText").GetComponent<Text>();
 		GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
 		Time.timeScale = 0;
+		if (loadDiaryName != null){
+			TextAsset asset = Resources.Load("data/diaries/"+loadDiaryName) as TextAsset;
+			diaryText.text = asset.text;
+		}
 	}
 	public void OKButtonCallback(){
 		Time.timeScale = 1;
