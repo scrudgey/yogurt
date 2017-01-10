d3fa5a2732a32f461d70d486fb901e85a27d9ecc
RSHAW <scrudgey@gmail.com>
2017-01-10 12:46:07 -0800

    initial commit

---
+++ UI/SpeechMenu.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class SpeechMenu : MonoBehaviour {
+    public void Start(){
+        GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
+    }
+    public void LineButton(){
+        MessageSpeech message = new MessageSpeech();
+        message.sayLine = true;
+        Toolbox.Instance.SendMessage(GameManager.Instance.playerObject, this, message);
+        Destroy(gameObject);
+    }
+    public void SwearButton(){
+        MessageSpeech message = new MessageSpeech();
+        message.randomSwear = true;
+        Toolbox.Instance.SendMessage(GameManager.Instance.playerObject, this, message);
+        Destroy(gameObject);
+    }
+    
+    public void SwearAtButton(){
+        Controller.Instance.currentSelect = Controller.SelectType.swearAt;
+        Destroy(gameObject);
+    }
+    
+    public void RandomButton(){
+        MessageSpeech message = new MessageSpeech();
+        message.randomSpeech = true;
+        Toolbox.Instance.SendMessage(GameManager.Instance.playerObject, this, message);
+        Destroy(gameObject);
+    }
+    
+    public void CancelButton(){
+        Destroy(gameObject);
+    }
+    
+}
