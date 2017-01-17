e288c87b91d22dff68896a9fb83e0a36495a27c9
RSHAW <scrudgey@gmail.com>
2017-01-16 17:11:28 -0800

    working monologue system, working callback for interaction via insult or threat

--- UI/DialogueMenu.cs
+++ UI/DialogueMenu.cs
@@ -4,22 +4,35 @@ using System.Collections.Generic;
 
 public class Monologue{
 	public Stack<string> text = new Stack<string>();
-	public GameObject speaker;
-
+	public Speech speaker;
+	public int index;
+	private string currentString;
 	public Monologue(){}
-	public Monologue(GameObject speaker, string[] texts){
+	public Monologue(Speech speaker, string[] texts){
+		index = 0;
 		this.speaker = speaker;
 		foreach(string entry in texts){
 			text.Push(entry);
 		}
 	}
+	public string GetString(){
+		currentString = text.Peek().Substring(0, index);
+		index += 1;
+		return Toolbox.Instance.CloneRemover(speaker.name) + ": " + currentString;
+	}
+	public bool MoreToSay(){
+		return currentString != text.Peek();
+	}
+	public void NextLine(){
+		index = 0;
+		text.Pop();
+	}
 }
 
 public class DialogueMenu : MonoBehaviour {
 	private AudioSource audioSource;
-	public GameObject instigator;
-	public GameObject target;
-	public GameObject speaker;
+	public Speech instigator;
+	public Speech target;
 
 	public Inventory instigatorInv;
 	public Inventory targetInv;
@@ -31,32 +44,31 @@ public class DialogueMenu : MonoBehaviour {
 	public Text choice2Text;
 	public Text choice3Text;
 	public Text promptText;
+	public GameObject choicePanel;
+
 	public Monologue monologue = new Monologue();
 	public Stack<Monologue> dialogue = new Stack<Monologue>();
-	public string currentString;
 	public bool waitForKeyPress;
-	public float blitInterval = 0.1f;
+	public float blitInterval = 0.01f;
 	public float blitTimer;
-	public int index;
-	public AudioClip blitSound;
 	void Start () {
 		audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
-		portrait = transform.Find("base/main/feedback/portraitPanel/portrait").GetComponent<Image>();
-		speechText = transform.Find("base/main/feedback/portraitPanel/speechPanel/speechText").GetComponent<Text>();
-		promptText = transform.Find("base/main/feedback/portraitPanel/speechPanel/textPrompt").GetComponent<Text>();
+		portrait = transform.Find("base/main/portrait").GetComponent<Image>();
+		speechText = transform.Find("base/main/speechPanel/speechText").GetComponent<Text>();
+		promptText = transform.Find("base/main/speechPanel/textPrompt").GetComponent<Text>();
 		choice1Text = transform.Find("base/choicePanel/choice1/Text").GetComponent<Text>();
 		choice2Text = transform.Find("base/choicePanel/choice2/Text").GetComponent<Text>();
 		choice3Text = transform.Find("base/choicePanel/choice3/Text").GetComponent<Text>();
+		choicePanel = transform.Find("base/choicePanel").gameObject;
 		Controller.Instance.suspendInput = true;
+		promptText.text = "";
+		choicePanel.SetActive(false);
 	}
 
-	public void Configure(GameObject instigator, GameObject target){
+	public void Configure(Speech instigator, Speech target){
 		Start();
-		GameObject giveButton = transform.Find("base/main/buttons/Give").gameObject;
-		GameObject demandButton = transform.Find("base/main/buttons/Demand").gameObject;
-		// GameObject insultButton = transform.Find("base/main/buttons/Insult").gameObject;
-		// GameObject suggestButton = transform.Find("base/main/buttons/Suggest").gameObject;
-		// GameObject followButton = transform.Find("base/main/buttons/Follow").gameObject;
+		GameObject giveButton = transform.Find("base/buttons/Give").gameObject;
+		GameObject demandButton = transform.Find("base/buttons/Demand").gameObject;
 		this.instigator = instigator;
 		this.target = target;
 		instigatorInv = instigator.GetComponent<Inventory>();
@@ -70,7 +82,7 @@ public class DialogueMenu : MonoBehaviour {
 	}
 
 	public void ChoiceCallback(int choiceNumber){
-		Say(gameObject, "choice number "+choiceNumber.ToString());
+		Say(instigator, "choice number "+choiceNumber.ToString());
 	}
 	public void ActionCallback(string callType){
 		switch (callType){
@@ -79,25 +91,29 @@ public class DialogueMenu : MonoBehaviour {
 			Controller.Instance.suspendInput = false;
 			break;
 			case "insult":
-			Monologue newLogue = new Monologue(instigator, new string[]{"thou insolent rogue!", "thou churlish knave!"});
-			Say(newLogue);
-			targetAwareness.Insulted(instigator, this);
+			Say(instigator.Insult(target.gameObject));
+			MessageInsult insult = new MessageInsult();
+			Toolbox.Instance.SendMessage(target.gameObject, instigator, insult);
+			Say(target.Riposte());
+			break;
+			case "threat":
+			Say(instigator.MonologueFromNimrod("threat", target.gameObject));
+			MessageThreaten threat = new MessageThreaten();
+			Toolbox.Instance.SendMessage(target.gameObject, instigator, threat);
+			Say(target.RespondToThreat());
 			break;
 			default:
 			break;
 		}
 	}
 
-	public void Say(GameObject speaker, string text){
+	public void Say(Speech speaker, string text){
 		Monologue newLogue = new Monologue(speaker, new string[]{text});
 		Say(newLogue);
 	}
 	public void Say(Monologue text){
 		if (monologue.text.Count == 0){
 			monologue = text;
-			index = 0;
-			speaker = text.speaker;
-			Debug.Log(speaker);
 		} else {
 			dialogue.Push(text);
 		}
@@ -109,12 +125,10 @@ public class DialogueMenu : MonoBehaviour {
 			if (waitForKeyPress){
 				waitForKeyPress = false;
 				if (monologue.text.Count > 0){
-					monologue.text.Pop();
+					monologue.NextLine();
 				} else if (dialogue.Count > 0){
 					monologue = dialogue.Pop();
 				}
-				index = 0;
-				speaker = monologue.speaker;
 				promptText.text = "";
 			}
 		}
@@ -132,12 +146,11 @@ public class DialogueMenu : MonoBehaviour {
 			return;
 		}
 		blitTimer = 0;
-		if (currentString != monologue.text.Peek()){
-			currentString = monologue.text.Peek().Substring(0, index);
-			index += 1;
-			speechText.text = speaker.name + ": " + currentString;
-			if (blitSound != null){
-				audioSource.PlayOneShot(blitSound);
+		if (monologue.MoreToSay()){
+			speechText.text = monologue.GetString();
+			if (monologue.speaker != null){
+				if (monologue.speaker.speakSound)
+					audioSource.PlayOneShot(monologue.speaker.speakSound);
 			}
 		} else {
 			if (monologue.text.Count > 1){
