7676b358a8411eec450d36fa5f76d7e22ac60a60
RSHAW <scrudgey@gmail.com>
2017-01-22 20:25:36 -0800

    made cutscenes generic. added mayor cutscene. changed new day cutscene to new method

--- cutscene/CutsceneManager.cs
+++ cutscene/CutsceneManager.cs
@@ -1,10 +1,55 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using Easings;
+using UnityEngine.SceneManagement;
+public class Cutscene {
+    public virtual void Update(){}
+    public virtual void Configure(){}
+    public bool complete;
+}
+public class CutsceneMayor : Cutscene {
+    private GameObject spawnPoint;
+    private GameObject mayor;
+    private Humanoid mayorControl;
+    private DecisionMaker mayorAI;
+    private Speech mayorSpeech;
+    private bool inPosition;
+    private bool walkingAway;
+    public override void Configure(){
+        spawnPoint = GameObject.Find("mayorSpawnpoint");
+        mayor = GameObject.Instantiate(Resources.Load("prefabs/Mayor"), spawnPoint.transform.position, Quaternion.identity) as GameObject;
+        mayorControl = mayor.GetComponent<Humanoid>();
+        mayorAI = mayor.GetComponent<DecisionMaker>();
+        mayorSpeech = mayor.GetComponent<Speech>();
 
-public class CutsceneManager : MonoBehaviour {
-    public enum CutsceneType {newDay}
-    public CutsceneType type = CutsceneType.newDay;
+        mayorAI.enabled = false;
+        Controller.Instance.suspendInput = true;
+    }
+    public override void Update(){
+        if (!inPosition){
+            mayorControl.rightFlag = true;
+        }
+        if (!inPosition && Vector3.Distance(mayor.transform.position, GameManager.Instance.playerObject.transform.position) < 0.25){
+            inPosition = true;
+            mayorControl.ResetInput();
+            DialogueMenu menu = mayorSpeech.SpeakWith();
+            menu.menuClosed += MenuWasClosed;
+        }
+        if (walkingAway){
+            mayorControl.leftFlag = true;
+            if (mayor.transform.position.x < spawnPoint.transform.position.x){
+                Object.Destroy(mayor);
+                Controller.Instance.suspendInput = false;
+                complete = true;
+            }
+        }
+    }
+
+    public void MenuWasClosed(){
+        walkingAway = true;
+    }
+}
+public class CutsceneNewDay : Cutscene {
     private float timer;
     Text tomText;
     Text dayText;
@@ -13,22 +58,18 @@ public class CutsceneManager : MonoBehaviour {
     private float startDayFade = 1.5f;
     private float stopDayFade = 2.5f;
     private float stopTime = 12f;
+    public override void Configure(){
+        canvas = GameObject.Find("Canvas");
+        tomText = canvas.transform.Find("tomText").GetComponent<Text>();
+        dayText = canvas.transform.Find("dayText").GetComponent<Text>();
 
-    void Start(){
-        if (type == CutsceneType.newDay){
-            canvas = GameObject.Find("Canvas");
-            tomText = canvas.transform.Find("tomText").GetComponent<Text>();
-            dayText = canvas.transform.Find("dayText").GetComponent<Text>();
+        dayText.text = "Day "+GameManager.Instance.data.days.ToString();
 
-            dayText.text = "Day "+GameManager.Instance.data.days.ToString();
-
-            Color blank = new Color(255, 255, 255, 0);
-            tomText.color = blank;
-            dayText.color = blank;
-        }
+        Color blank = new Color(255, 255, 255, 0);
+        tomText.color = blank;
+        dayText.color = blank;
     }
-
-    void Update(){
+    public override void Update(){
         timer += Time.deltaTime;
         if (timer < stopTomFade){
             Color col = tomText.color;
@@ -41,8 +82,50 @@ public class CutsceneManager : MonoBehaviour {
             dayText.color = col;
         }
         if (timer >= stopTime || Input.anyKey){
+            complete = true;
             GameManager.Instance.NewDay();
         }
     }
-    
+}
+
+public class CutsceneManager : Singleton<CutsceneManager> {
+    public enum CutsceneType {newDay, mayorTalk}
+    public Cutscene cutscene;
+    void Start (){
+        SceneManager.sceneLoaded += LevelWasLoaded;
+    }
+    public void InitializeCutscene(CutsceneType scene){
+        switch (scene)
+        {
+            case CutsceneType.newDay:
+            cutscene = new CutsceneNewDay();
+            break;
+            case CutsceneType.mayorTalk:
+            cutscene = new CutsceneMayor();
+            cutscene.Configure();
+            break;
+            default:
+            break;
+        }
+    }
+    public void LevelWasLoaded(Scene scene, LoadSceneMode mode){
+        if (cutscene == null)
+            return;
+        if (cutscene.complete){
+            cutscene = null;
+            return;
+        }
+        if (cutscene is CutsceneNewDay)
+            cutscene.Configure();
+    }
+
+    void Update(){
+        if (cutscene == null)
+            return;
+        if (cutscene.complete){
+            cutscene = null;
+        } else {
+            cutscene.Update();
+        }
+    }
 }
