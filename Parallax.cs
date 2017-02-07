43763f560b3a7a3daf47345b5630be68eba99f1f
RSHAW <scrudgey@gmail.com>
2017-02-06 21:34:29 -0800

    working parallax- but speed is inverted. may change this later

--- Parallax.cs
+++ Parallax.cs
@@ -2,39 +2,32 @@
 using System;
 using System.Collections.Generic;
 // using System.Collections;
-[Serializable]
-public struct ParallaxLayer {
-	public Vector3 originalPosition;
-	public Transform transform;
-	public float speed;
-}
+
+
 public class Parallax : MonoBehaviour {
+	[Serializable]
+	public struct ParallaxLayer {
+		public Vector2 originalPosition;
+		public Transform transform;
+		public float speed;
+	}
 	public Transform baseLayer;
-	public Vector3 originalPosition;
+	public Vector2 originalPosition;
 	public List<ParallaxLayer> layers = new List<ParallaxLayer>();
-	// public SerializableDictionary<Transform, float> layerSpeeds = new SerializableDictionary<Transform, float>();
 	void Start(){
 		Transform cameraTransform = GameObject.FindObjectOfType<Camera>().transform;
-		// Debug
 		Debug.Log(cameraTransform.position);
 		originalPosition = cameraTransform.position;
-		// originalPosition = baseLayer.transform.position;
-		// Transform cameraTransform = GameManager.Instance.cam.transform;
-		// originalPosition = new Vector3(cameraTransform.position.x, cameraTransform.position.y, cameraTransform.position.z);
-		// foreac(ParallaxLayer layer in layers){
 		for (int i = 0; i < layers.Count; i++){
 			ParallaxLayer layer = layers[i];
-			// layer.originalPosition = layers[i].transform.TransformPoint(Vector2.zero);
 			layer.originalPosition = layers[i].transform.position;
 			layers[i] = layer;
 		}
 	}
 	void Update () {
-		Vector3 difference = GameManager.Instance.cam.transform.position - originalPosition;
+		Vector3 difference = (Vector2)GameManager.Instance.cam.transform.position - originalPosition;
 		foreach(ParallaxLayer layer in layers){
-			// layer.transform.position = layer.originalPosition + difference * layer.speed;
-			layer.transform.position = baseLayer.position - difference * layer.speed;
-			// layer.transform.position.z = 0;
+			layer.transform.position = baseLayer.position + difference * layer.speed;
 		}
 	}
 }
