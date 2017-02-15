c028fdf5b1627a7b034fdbc00c0597affa119109
RSHAW <scrudgey@gmail.com>
2017-02-14 16:39:02 -0800

    initial commit

---
+++ itemcode/DoorwayZone.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+// using System.Collections;
+
+public class DoorwayZone : Doorway {
+	public Transform enterPoint;
+	public override void Start () {
+		// enterPoint = transform.Find("enterPoint");
+		audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
+	}
+	public override void Enter(GameObject player){
+		enterPoint = transform.Find("enterPoint");
+		player.transform.position = enterPoint.position;
+		PlayEnterSound();
+	}
+	void OnTriggerEnter2D(Collider2D collider){
+		if (collider.gameObject == GameManager.Instance.playerObject){
+			Leave();
+		}
+	}
+}
