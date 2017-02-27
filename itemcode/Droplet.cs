451a70d61feb1e1befeaa188d0c503aba7ac6b05
RSHAW <scrudgey@gmail.com>
2017-02-27 00:15:50 -0800

    changed layers scheme

--- itemcode/Droplet.cs
+++ itemcode/Droplet.cs
@@ -5,7 +5,7 @@ public class Droplet : MonoBehaviour {
 	void OnGroundImpact(Physical phys){
 		MonoLiquid monoLiquid = GetComponent<MonoLiquid>();
 		GameObject puddle = Instantiate(Resources.Load("Puddle"), transform.position, Quaternion.identity) as GameObject;
-		puddle.layer = 8;
+		puddle.layer = 9;
 		PhysicalBootstrapper pb = GetComponent<PhysicalBootstrapper>();
 		pb.DestroyPhysical();
 		LiquidCollection.MonoLiquidify(puddle, monoLiquid.liquid);
