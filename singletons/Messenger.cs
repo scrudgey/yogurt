57c743bc2dce4f9eb8cad4f40393c9d97c0a58ac
RSHAW <scrudgey@gmail.com>
2017-03-31 18:22:59 -0700

    linting

--- singletons/Messenger.cs
+++ singletons/Messenger.cs
@@ -3,21 +3,16 @@
 using System.Collections.Generic;
 
 public class Messenger : Singleton<Messenger> {
-
 	public string MOTD = "smoke weed every day";
 	public Dictionary<GameObject, IExcludable> claimedItems = new Dictionary<GameObject, IExcludable>();
-
 	void Start(){
 		claimedItems = new Dictionary<GameObject, IExcludable>();
 	}
-
 	public void ListObjects(){
 		foreach (GameObject o in claimedItems.Keys){
 			Debug.Log(o.name);
 		}
 	}
-
-
 	public void ClaimObject(GameObject obj, IExcludable owner){
 		// if someone else owns the object, tell them that it's being taken.
 		if (claimedItems.ContainsKey(obj)){
@@ -25,18 +20,15 @@ public class Messenger : Singleton<Messenger> {
 		}
 		claimedItems[obj] = owner;
 	}
-
 	public void DisclaimObject(GameObject obj, IExcludable owner){
 		if (claimedItems.ContainsKey(obj))
 			if (claimedItems[obj] == owner)
 				claimedItems.Remove(obj);
 	}
-
 	public void WasDestroyed(GameObject obj){
 		if (claimedItems.ContainsKey(obj)){
 			claimedItems[obj].WasDestroyed(obj);
 		}
 		Destroy(obj);
 	}
-
 }
