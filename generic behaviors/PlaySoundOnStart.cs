1d763423518fa65dc1363e6bcd8a58f925957712
RSHAW <scrudgey@gmail.com>
2017-03-31 18:32:44 -0700

    allowing flag for disabling spatial blending

--- generic behaviors/PlaySoundOnStart.cs
+++ generic behaviors/PlaySoundOnStart.cs
@@ -2,8 +2,11 @@
 public class PlaySoundOnStart : MonoBehaviour {
 	public AudioClip sound;
 	private AudioSource source;
+	public bool disableSpatialBlending;
 	void Start () {
 		source = Toolbox.Instance.SetUpAudioSource(gameObject);
+		if (disableSpatialBlending)
+			source.spatialBlend = 0;
 		if (sound != null)
 			source.PlayOneShot(sound);
 	}
