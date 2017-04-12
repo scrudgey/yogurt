2cb1b244d2433314f6d9bef10d902ac343080c22
RSHAW <scrudgey@gmail.com>
2017-04-12 10:56:21 -0700

    initial commit

---
+++ Spawner.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Spawner : MonoBehaviour {
+	public GameObject target;
+	private float timer;
+	public float delayTime;
+	void Update () {
+		timer += Time.deltaTime;
+		if (delayTime == 0){
+			timer = -1;
+			Destroy(gameObject);
+		}
+		if (timer > delayTime){
+			Instantiate(target, transform.position, Quaternion.identity);
+			Destroy(gameObject);
+		}
+	}
+}
