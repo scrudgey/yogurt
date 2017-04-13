b01311d6d2220e4cc7ad16577decb78d152e0317
RSHAW <scrudgey@gmail.com>
2017-04-13 12:25:10 -0700

    working version of telephone calling FD

--- specialObjects/Telephone.cs
+++ specialObjects/Telephone.cs
@@ -1,9 +1,9 @@
-﻿// using System.Collections;
-// using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Telephone : Item {
 	AudioSource source;
+	public Doorway enterDoor;
+	public float fireTime;
 	public AudioClip phoneUp;
 	// public AudioClip dialTone;
 	public AudioClip phoneDown;
@@ -22,5 +22,29 @@ public class Telephone : Item {
 	}
 	public string UsePhone_desc(){
 		return "Use telephone";
+	}
+	public void FireButtonCallback(){
+		if (fireTime <= 0){
+			MessageSpeech message = new MessageSpeech();
+			message.phrase = "We'll send someone over right away!";
+			Toolbox.Instance.SendMessage(gameObject, this, message);
+			fireTime = 1f;
+		} else {
+			MessageSpeech message = new MessageSpeech();
+			message.phrase = "Just sit tight, the team is on their way!";
+			Toolbox.Instance.SendMessage(gameObject, this, message);
+		}
+	}
+	public void Update(){
+		if (fireTime > 0){
+			fireTime -= Time.deltaTime;
+			if (fireTime <= 0){
+				//do fireman spawn
+				Vector3 tempPos = enterDoor.transform.position;
+				tempPos.y = tempPos.y - 0.05f;
+				Instantiate(Resources.Load("prefabs/Fireman"), tempPos, Quaternion.identity);
+				enterDoor.PlayEnterSound();
+			}
+		}
 	}
 }
