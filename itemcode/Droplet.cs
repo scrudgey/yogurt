e4913971f2d7f19b61ec3a18947ff774a33a0d8e
RSHAW <scrudgey@gmail.com>
2017-04-15 20:51:03 -0700

    added splashing code to droplet, although i think this script is unused

--- itemcode/Droplet.cs
+++ itemcode/Droplet.cs
@@ -2,6 +2,10 @@
 // using System.Collections;
 
 public class Droplet : MonoBehaviour {
+	public Physical physical;
+	void Awake(){
+		physical = GetComponent<Physical>();
+	}
 	void OnGroundImpact(Physical phys){
 		MonoLiquid monoLiquid = GetComponent<MonoLiquid>();
 		GameObject puddle = Instantiate(Resources.Load("Puddle"), transform.position, Quaternion.identity) as GameObject;
@@ -9,5 +13,10 @@ public class Droplet : MonoBehaviour {
 		PhysicalBootstrapper pb = GetComponent<PhysicalBootstrapper>();
 		pb.DestroyPhysical();
 		LiquidCollection.MonoLiquidify(puddle, monoLiquid.liquid);
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if (physical.currentMode == Physical.mode.fly){
+			Debug.Log("droplet fly collision");
+		}
 	}
 }
