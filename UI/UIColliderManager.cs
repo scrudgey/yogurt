9af655b7cc7f8626cb94c71c88092213169a9d67
RSHAW <scrudgey@gmail.com>
2017-04-22 17:16:44 -0700

    initial commit

---
+++ UI/UIColliderManager.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class UIColliderManager : MonoBehaviour {
+	public BoxCollider2D leftBox;
+	public BoxCollider2D rightBox;
+	public BoxCollider2D topBox;
+	public BoxCollider2D bottomBox;
+	public RectTransform rt;
+	void Start (){
+		rt = GetComponent<RectTransform>();
+		Vector2 leftSize = new Vector2(10, rt.rect.height);
+		Vector2 leftOffset = new Vector2(-1f * rt.rect.width / 2f, 0);
+
+		Vector2 rightSize = new Vector2(10, rt.rect.height);
+		Vector2 rightOffset = new Vector2(rt.rect.width / 2f, 0);
+
+		Vector2 topSize = new Vector2(rt.rect.width, 10);
+		Vector2 topOffset = new Vector2(0, rt.rect.height / 2f);
+
+		Vector2 bottomSize = new Vector2(rt.rect.width, 10);
+		Vector2 bottomOffset = new Vector2(0, -1f * rt.rect.height / 2f);
+
+		leftBox.size = leftSize;
+		leftBox.offset = leftOffset;
+		rightBox.size = rightSize;
+		rightBox.offset = rightOffset;
+		bottomBox.offset = bottomOffset;
+		bottomBox.size = bottomSize;
+		topBox.size = topSize;
+		topBox.offset = topOffset;
+	}
+	// void Update () {
+	// 	Vector2 leftSize = new Vector2(10, rt.rect.height);
+	// 	Vector2 leftOffset = new Vector2(-1f * rt.rect.width / 2f, 0);
+
+	// 	Vector2 rightSize = new Vector2(10, rt.rect.height);
+	// 	Vector2 rightOffset = new Vector2(rt.rect.width / 2f, 0);
+
+	// 	Vector2 topSize = new Vector2(rt.rect.width, 10);
+	// 	Vector2 topOffset = new Vector2(0, rt.rect.height / 2f);
+
+	// 	Vector2 bottomSize = new Vector2(rt.rect.width, 10);
+	// 	Vector2 bottomOffset = new Vector2(0, -1f * rt.rect.height / 2f);
+
+	// 	leftBox.size = leftSize;
+	// 	leftBox.offset = leftOffset;
+	// 	rightBox.size = rightSize;
+	// 	rightBox.offset = rightOffset;
+	// 	bottomBox.offset = bottomOffset;
+	// 	bottomBox.size = bottomSize;
+	// 	topBox.size = topSize;
+	// 	topBox.offset = topOffset;
+	// }
+}
