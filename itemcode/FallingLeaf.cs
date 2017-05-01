3cc5c4497550ce42c19d0f26d8155a93452e04c8
RSHAW <scrudgey@gmail.com>
2017-05-01 11:37:57 -0700

    amplitude variation and falling speed oscillation

--- itemcode/FallingLeaf.cs
+++ itemcode/FallingLeaf.cs
@@ -8,23 +8,25 @@ public class FallingLeaf : MonoBehaviour {
 	public Sprite finalSprite;
 	private AnimateFrames animate;
 	private float period;
-	private float initX;
+	private float xInit;
+	private float xAmplitude;
 	private float timer;
 	void Start(){
 		animate = GetComponent<AnimateFrames>();
 		period = animate.frameTime * 4f;
-		initX = transform.position.x;
+		xInit = transform.position.x;
+		xAmplitude = Random.Range(0.05f, 0.1f);
 	}
 	void Update () {
 		if (height > 0){
 			timer += Time.deltaTime;
 			Vector3 newPos = transform.position;
-			newPos.y -= fallSpeed;
-			newPos.x = initX + 0.05f * Mathf.Cos(timer * (2f * 3.14f) / period);
+			newPos.y -= fallSpeed * Mathf.Abs(Mathf.Sin(timer * (2f * 3.14f) / period));
+			newPos.x = xInit + xAmplitude * Mathf.Cos(timer * (2f * 3.14f) / period);
 			// Debug.Log(period);
 			// Debug.Log(Mathf.Cos((2f * 3.14f) / period));
 			transform.position = newPos;
-			height -= fallSpeed;
+			height -= fallSpeed * Mathf.Abs(Mathf.Sin(timer * (2f * 3.14f) / period));
 		} else {
 			FinalState();
 		}
