28748b1dc37b5a0f09e58804a49331984c42be01
RSHAW <scrudgey@gmail.com>
2017-05-01 11:36:29 -0700

    new types of liquid. start to awake

--- generics/LiquidResevoir.cs
+++ generics/LiquidResevoir.cs
@@ -4,26 +4,29 @@ public class LiquidResevoir : Interactive {
 	public AudioClip fillSound;
 	public enum liquidType{
 		water,
-		test2
+		drainCleaner,
+		toiletWater
 	}
 
 	public liquidType containedLiquid;
 	public string type;
 
-	 void Start () {
+	 void Awake () {
 
 		switch (containedLiquid){
 		case liquidType.water:
 			type = "water";
 			break;
-		case liquidType.test2:
+		case liquidType.drainCleaner:
 			type = "drain cleaner";
+			break;
+		case liquidType.toiletWater:
+			type = "toilet water";
 			break;
 		default:
 			type = "default";
 			break;
 		}
-
 		liquid = LiquidCollection.LoadLiquid(type);
 	}
 
