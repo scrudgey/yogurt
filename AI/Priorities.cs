09a9418eada1d779856023e7a5232fcf200d3b91
RSHAW <scrudgey@gmail.com>
2017-06-19 16:15:11 -0700

    protect possessions priority

--- AI/Priorities.cs
+++ AI/Priorities.cs
@@ -69,15 +69,32 @@ namespace AI{
 		}
 	}
 	public class PriorityProtectPossessions : Priority {
+		Ref<Vector2> returnPosition = new Ref<Vector2>(Vector2.zero);
+		Ref<GameObject> possession = new Ref<GameObject>(null);
 		public PriorityProtectPossessions(GameObject g, Controllable c): base(g, c){
-			Goal gainPossession = new GoalGetItem(gameObject, control, awareness.possession);
+			Goal findPossession = new GoalWander(gameObject, control);
+			findPossession.successCondition = new ConditionSawObjectRecently(gameObject, control, possession);
+			
+			Goal gainPossession = new GoalGetItem(gameObject, control, possession);
+			Goal walkToReturnPoint = new GoalWalkToPoint(gameObject, control, returnPosition);
 			Goal returnObject = new Goal(gameObject, control);
-			returnObject.requirements.Add(gainPossession);
+			returnObject.routines.Add(new RoutinePlaceObject(gameObject, control, returnPosition));
+			returnObject.successCondition = new ConditionObjectInPlace(gameObject, control, possession, returnPosition);
+
+			returnObject.requirements.Add(walkToReturnPoint);
+			walkToReturnPoint.requirements.Add(gainPossession);
+			gainPossession.requirements.Add(findPossession);
+			
+			goal = returnObject;
 		}
 		public override void Update(){
-			// if (awareness.possession == null){
-			// 	urgency = 0;
-			// }
+			if (awareness.PossessionsAreOkay()){
+				urgency = 0;
+			} else {
+				urgency = urgencyLarge;
+				possession.val = awareness.possession;
+				returnPosition.val = awareness.possessionDefaultState.lastSeenPosition;
+			}
 		}
 	}
 
