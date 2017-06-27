636423afef7e069ddec7c87335dd9970e0c2d958
RSHAW <scrudgey@gmail.com>
2017-06-27 14:33:26 -0700

    linting

--- UI/CommercialReportMenu.cs
+++ UI/CommercialReportMenu.cs
@@ -3,7 +3,6 @@ using UnityEngine.UI;
 
 public class CommercialReportMenu : MonoBehaviour {
     Text descriptionText;
-    // Text rewardText;
     Text positiveScore, chaosScore, disgustingScore, disturbingScore, offensiveScore;
     Text transcript;
     Text eventText;
@@ -14,7 +13,6 @@ public class CommercialReportMenu : MonoBehaviour {
         SetRefs();
         Controller.Instance.suspendInput = true;
     }
-
     public void ContinueButton(){
         Controller.Instance.suspendInput = false;
         Destroy(gameObject);
@@ -27,14 +25,11 @@ public class CommercialReportMenu : MonoBehaviour {
         UINew.Instance.CloseActiveMenu();
         Controller.Instance.suspendInput = false;
 		MySaver.Save();
-        // GameManager.Instance.NewDayCutscene();
         GameManager.Instance.BoardRoomCutscene();
     }
-    
     private void SetRefs(){
         if (!descriptionText){
             descriptionText = transform.Find("Image/desc/WhatText").GetComponent<Text>();
-            // rewardText = transform.Find("Image/buttons/rew/RewardText").GetComponent<Text>();
             positiveScore = transform.Find("Image/Center/RightHalf/RatingsPanel/Positive/Amount").GetComponent<Text>();
             chaosScore = transform.Find("Image/Center/RightHalf/RatingsPanel/Chaos/Amount").GetComponent<Text>();
             disgustingScore = transform.Find("Image/Center/RightHalf/RatingsPanel/Disgusting/Amount").GetComponent<Text>();
@@ -44,23 +39,17 @@ public class CommercialReportMenu : MonoBehaviour {
             eventText = transform.Find("Image/Center/RightHalf/EventsPanel/Text").GetComponent<Text>();
         }
     }
-    
     public void Report(Commercial activeCommercial, Commercial commercial){
         SetRefs();
-        // titleText.text = commercial.name;
         descriptionText.text = activeCommercial.description;
-        // rewardText.text = activeCommercial.reward.ToString();
-
         positiveScore.text = commercial.data.positive.ToString();
         chaosScore.text = commercial.data.chaos.ToString();
         disgustingScore.text = commercial.data.disgusting.ToString();
         disturbingScore.text = commercial.data.disturbing.ToString();
         offensiveScore.text = commercial.data.offensive.ToString();
-
         foreach (string line in commercial.transcript){
             transcript.text = transcript.text + line + "\n";
         }
-
         eventText.text = "";
         foreach (string key in commercial.properties.Keys){
             CommercialProperty prop = commercial.properties[key];
@@ -69,5 +58,4 @@ public class CommercialReportMenu : MonoBehaviour {
             eventText.text = eventText.text + line;
         }
     }
-    
 }
