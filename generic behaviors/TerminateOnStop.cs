b5f78c8729489cc372e34b6841dde154f4891f5e
RSHAW <scrudgey@gmail.com>
2017-06-27 14:30:46 -0700

    linting, move obj to bkg when terminate

--- generic behaviors/TerminateOnStop.cs
+++ generic behaviors/TerminateOnStop.cs
@@ -1,24 +1,14 @@
 ﻿using UnityEngine;
-// using System.Collections;
-
-//[RequireComponent (typeof (Rigidbody2D))]
 
 public class TerminateOnStop : MonoBehaviour {
-
-	// Use this for initialization
-//	void Start () {
-//	
-//	}
-	
-	// Update is called once per frame
 	void Update () {
-
 		if (GetComponent<Rigidbody2D>().velocity.magnitude < 0.01){
-
 			if (GetComponent<Rigidbody2D>())
 				Destroy(GetComponent<Rigidbody2D>());
 			if (GetComponent<Collider2D>())
 				Destroy (GetComponent<Collider2D>());
+			SpriteRenderer renderer = GetComponent<SpriteRenderer>();
+			renderer.sortingLayerName = "background";
 			Destroy(this);
 		}
 	}
