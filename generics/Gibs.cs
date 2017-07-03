2d31792d837131cb41c5d6dca28880452e5e2045
RSHAW <scrudgey@gmail.com>
2017-07-03 16:24:22 -0700

    gibs color

--- generics/Gibs.cs
+++ generics/Gibs.cs
@@ -1,30 +1,25 @@
 ﻿using UnityEngine;
-// using System.Collections;
 
 public class Gibs : MonoBehaviour {
-
 	public damageType damageCondition;
 	public int number;
 	public GameObject particle;
 	public float forceMin;
 	public float forceMax;
-
+	public Color color = Color.white;
 	public void Emit(){
 		for (int i =0; i<number; i++){
-
 			GameObject bit = Instantiate(particle) as GameObject;
 			bit.transform.position = transform.position;
 			Rigidbody2D bitPhys = bit.GetComponent<Rigidbody2D>();
-
 			if (bitPhys){
 				Vector2 force = Random.insideUnitCircle;
 				force = force * (forceMax * Random.value + forceMin );
 				bitPhys.AddForce(force);
 			}
-
-			// SpriteRenderer sprite = bit.GetComponent<SpriteRenderer>();
+			SpriteRenderer sprite = bit.GetComponent<SpriteRenderer>();
+			sprite.color = color;
 			// sprite.sortingLayerName = "background";
-
 		}
 	}
 }
