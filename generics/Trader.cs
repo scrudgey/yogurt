a80c6eb2557078e28d87321354a771c3eecff529
RSHAW <scrudgey@gmail.com>
2017-07-03 17:00:06 -0700

    changed variable names. item to receive is now a string of a name instead of a prefab for simplicity

--- generics/Trader.cs
+++ generics/Trader.cs
@@ -1,8 +1,8 @@
 ﻿using UnityEngine;
 
 public class Trader : Interactive {
-	public GameObject itemForSale;
-	public GameObject itemToBuy;
+	public GameObject give;
+	public string receive;
 	private Inventory inv;
 	private Awareness aware;
 	void Start(){
@@ -13,46 +13,41 @@ public class Trader : Interactive {
 	// TODO: adjust responses, incorporate nimrod
 	public void Trade(Inventory other){
 		// item is sold, or missing
-		if (itemForSale == null){
-			// if (speak)
-				Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("I have nothing to sell!")); 
+		if (give == null){
+			Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("I have nothing to sell!")); 
 			return;
 		}
 		// player presents an item
 		if (other.holding){
 			// success
-			if (Toolbox.Instance.CloneRemover(other.holding.name) == Toolbox.Instance.CloneRemover(itemToBuy.name)){
+			if (Toolbox.Instance.CloneRemover(other.holding.name) == receive){
 				Exchange(other, other.holding);
-				// if (speak)
-					Toolbox.Instance.SendMessage(other.gameObject, this, new MessageSpeech("I bought it!")); 
+				Toolbox.Instance.SendMessage(other.gameObject, this, new MessageSpeech("I bought it!")); 
 				return;
 			}
 			// holding the wrong item
-			// if (speak)
-				Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("I don't want that!")); 
+			Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("I don't want that!")); 
 			return;
 		}
 		foreach (GameObject item in other.items){
 			// player has the item, but it is stashed
-			if (Toolbox.Instance.CloneRemover(item.name) == Toolbox.Instance.CloneRemover(itemToBuy.name)){
-				// if (speak)
-					Toolbox.Instance.SendMessage(other.gameObject, this, new MessageSpeech("Hold on, let me find it...")); 
+			if (Toolbox.Instance.CloneRemover(item.name) == Toolbox.Instance.CloneRemover(receive)){
+				Toolbox.Instance.SendMessage(other.gameObject, this, new MessageSpeech("Hold on, let me find it...")); 
 				return;
 			}
 		}
 		// player does not have the correct item at all
-		// if (speak)
-			Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("Give me one "+itemToBuy.name)); 
+		Toolbox.Instance.SendMessage(gameObject, this, new MessageSpeech("Give me one "+receive)); 
 	}
 	public enum TradeStatus {none, pass, noItemForTrade, noItemOffered, wrongItemOffered}
 	public TradeStatus CheckTradeStatus(Inventory other){
-		if (itemForSale == null){
+		if (give == null){
 			return TradeStatus.noItemForTrade;
 		}
 		// player presents an item
 		if (other.holding){
 			// success
-			if (Toolbox.Instance.CloneRemover(other.holding.name) == Toolbox.Instance.CloneRemover(itemToBuy.name)){
+			if (Toolbox.Instance.CloneRemover(other.holding.name) == receive){
 				return TradeStatus.pass;
 			}
 			// holding the wrong item
@@ -62,7 +57,7 @@ public class Trader : Interactive {
 		return TradeStatus.noItemOffered;
 	}
 	public string Trade_desc(Inventory other){
-		return "Trade a "+Toolbox.Instance.CloneRemover(itemToBuy.name)+ " for a "+Toolbox.Instance.CloneRemover(itemForSale.name);
+		return "Trade a "+Toolbox.Instance.CloneRemover(receive)+ " for a "+Toolbox.Instance.CloneRemover(give.name);
 	}
 	public void Exchange(Inventory otherInv){
 		if (otherInv.holding)
@@ -71,11 +66,11 @@ public class Trader : Interactive {
 	public void Exchange(Inventory otherInv, Pickup other){
 		aware = GetComponent<Awareness>();
 		if (aware){
-			if (aware.possession == itemForSale)
+			if (aware.possession == give)
 				aware.possession = null;
 		}
 		otherInv.SoftDropItem();
-		Pickup myPickup = itemForSale.GetComponent<Pickup>();
+		Pickup myPickup = give.GetComponent<Pickup>();
 		if (myPickup != null){
 			otherInv.GetItem(myPickup);
 		}
@@ -83,6 +78,6 @@ public class Trader : Interactive {
 		if (aware){
 			aware.possession = other.gameObject;
 		}
-		itemForSale = null;
+		give = null;
 	}
 }
