89f0f1f9eb76137e2d157a0c8cca86fc63e89ec6
RSHAW <scrudgey@gmail.com>
2017-07-12 10:53:27 -0700

    linting

--- savesystem/MyMarker.cs
+++ savesystem/MyMarker.cs
@@ -1,17 +1,12 @@
 ﻿using UnityEngine;
-// using System.Collections;
 
 public class MyMarker : MonoBehaviour {
-
-
 	void OnDisable(){
 		MySaver.disabledPersistents.Add(gameObject);
 	}
-
 	void OnEnable(){
 		MySaver.disabledPersistents.Remove(gameObject);
 	}
-
 	void OnDestroy(){
 		if (MySaver.disabledPersistents.Contains(gameObject)){
 			MySaver.disabledPersistents.Remove(gameObject);
