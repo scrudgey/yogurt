580dd4704eea9742c95cb5f0b2db683dc53520ed
RSHAW <scrudgey@gmail.com>
2017-08-05 13:12:08 -0700

    responsible party is holder

--- itemcode/Lighter.cs
+++ itemcode/Lighter.cs
@@ -1,32 +1,26 @@
 ﻿using UnityEngine;
-// using System.Collections;
 using System.Collections.Generic;
 
 public class Lighter : Interactive {
 	public ParticleSystem fire;
+	private Pickup pickup;
 	private bool flameon;
 	private Collider2D flameRadius;
-//	private List<Flammable> flammables;
-	private Dictionary<GameObject,Flammable> flammables = new Dictionary<GameObject,Flammable>();
+	private Dictionary<GameObject, Flammable> flammables = new Dictionary<GameObject, Flammable>();
 	private bool LoadInitialized = false;
-
 	void Start () {
-
-//		flameon = false;
 		if (!LoadInitialized)
 			LoadInit();
 	}
-
 	public void LoadInit(){
-		Interaction f = new Interaction(this,"Fire","Fire",false,true);
+		pickup = GetComponent<Pickup>();
+		Interaction f = new Interaction(this, "Fire", "Fire", false, true);
 		f.defaultPriority = 1;
 		interactions.Add(f);
-		flameRadius = transform.FindChild("flameRadius").GetComponent<Collider2D>();
+		flameRadius = transform.Find("flameRadius").GetComponent<Collider2D>();
 		flameRadius.enabled = false;
-
 		LoadInitialized = true;
 	}
-
 	public void Fire(){
 		flameon = !flameon;
 		if (flameon){
@@ -37,19 +31,21 @@ public class Lighter : Interactive {
 			flameRadius.enabled = false;
 		}
 	}
-
 	void OnTriggerEnter2D(Collider2D coll){
 		if (!flammables.ContainsKey(coll.gameObject)){
 			Flammable flammable = coll.GetComponent<Flammable>();
 			if (flammable != null){
-				flammables.Add(coll.gameObject,flammable);
+				flammables.Add(coll.gameObject, flammable);
 			}
 		}
-	}	
-
+	}
 	void OnTriggerStay2D(Collider2D coll){
 		if (flammables.ContainsKey(coll.gameObject) && flameRadius.enabled ){
 			flammables[coll.gameObject].heat += Time.deltaTime * 2f;
+			if (pickup != null){
+				if (pickup.holder != null)
+					flammables[coll.gameObject].responsibleParty = pickup.holder.gameObject;
+			}
 		}
 	}
 
