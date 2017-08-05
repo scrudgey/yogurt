574905bf3dec119270373b798216670e62c267b8
RSHAW <scrudgey@gmail.com>
2017-08-04 21:24:22 -0700

    initial commit

---
+++ Stain.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class Stain : MonoBehaviour {
+	public SpriteMask parentMask;
+	public SpriteRenderer parentRenderer;
+	void Update(){
+		if (parentRenderer != null){
+			parentMask.sprite = parentRenderer.sprite;
+		}
+	}
+}
