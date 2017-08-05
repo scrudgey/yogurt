99fdf79eedcdaa4d8a6c55ce611dbb1f022d066b
RSHAW <scrudgey@gmail.com>
2017-08-05 13:07:16 -0700

    linting

--- generics/Item.cs
+++ generics/Item.cs
@@ -1,9 +1,4 @@
-﻿// using UnityEngine;
-// using System.Collections;
-// using System;
-// using System.Collections.Generic;
-
-public class Item : Interactive {
+﻿public class Item : Interactive {
 	public string itemName;
 	public string description;
 	public string longDescription;
