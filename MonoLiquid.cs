df370e2d11c96ade9c8da7c27736accd01e31a5d
RSHAW <scrudgey@gmail.com>
2017-08-04 21:24:39 -0700

    splashes of puddles make stains

--- MonoLiquid.cs
+++ MonoLiquid.cs
@@ -3,6 +3,19 @@ public class MonoLiquid : MonoBehaviour {
 	public Liquid liquid;
 	public Edible edible;
 	public PhysicalBootstrapper physB;
+	static GameObject CreateStain(GameObject target, Vector3 location){
+		SpriteMask parentMask = Toolbox.Instance.GetOrCreateComponent<SpriteMask>(target);
+		SpriteRenderer parentRenderer = target.GetComponent<SpriteRenderer>();
+		GameObject stainObject = Instantiate(Resources.Load("stain")) as GameObject;
+		Stain stain = stainObject.GetComponent<Stain>();
+		stain.parentMask = parentMask;
+		stain.parentRenderer = parentRenderer;
+		stainObject.transform.position = location;
+		stainObject.transform.SetParent(target.transform, true);
+		SpriteRenderer stainRenderer = stainObject.GetComponent<SpriteRenderer>();
+		stainRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+		return stainObject;
+	}
 	void Awake(){
 		edible = GetComponent<Edible>();
 		if (!edible)
@@ -27,6 +40,11 @@ public class MonoLiquid : MonoBehaviour {
 			splashSprite.color = liquid.color;
 			Transform rectTransform = splash.GetComponent<Transform>();
  			rectTransform.Rotate(new Vector3(0, 0, Random.Range(0f, 360f)));
+
+			GameObject stain = CreateStain(coll.gameObject, transform.position);
+			SpriteRenderer stainRenderer = stain.GetComponent<SpriteRenderer>();
+			stainRenderer.color = liquid.color;
+			LiquidCollection.MonoLiquidify(stain, liquid);
 			Destroy(gameObject);
 		}
 	}
