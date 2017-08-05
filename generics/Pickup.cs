98e89880670e2ab7aadba2c737fcfd7328e5df14
RSHAW <scrudgey@gmail.com>
2017-08-05 13:10:23 -0700

    pickup knows its holder

--- generics/Pickup.cs
+++ generics/Pickup.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
-// using System.Collections;
-// using System.Collections.Generic;
 
 public class Pickup : Item {
 	public float weight;
 	public AudioClip[] pickupSounds;
+	public Inventory holder;
 }
