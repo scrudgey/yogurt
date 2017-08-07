8d01e4d69e9948e10e96c8e7e99ba70f36dd14a4
RSHAW <scrudgey@gmail.com>
2017-08-07 13:55:06 -0700

    initial commit

---
+++ itemcode/Projectile.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public float damage;
+	private MessageDamage message;
+	public AudioClip[] wallImpactSounds;
+	public AudioClip[] hurtableImpactSounds;
+	private Rigidbody2D body;
+	private RotateTowardMotion rotator;
+	void Awake(){
+		message = new MessageDamage(damage, damageType.physical);
+		body = GetComponent<Rigidbody2D>();
+		rotator = GetComponent<RotateTowardMotion>();
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		Hurtable hurtable = coll.gameObject.GetComponent<Hurtable>();
+		if (hurtable){
+			Toolbox.Instance.SendMessage(coll.gameObject, this, message);
+			if (hurtableImpactSounds.Length > 0){
+				Toolbox.Instance.AudioSpeaker(hurtableImpactSounds[Random.Range(0, hurtableImpactSounds.Length)], transform.position);
+			}
+			
+			hurtable.Bleed(transform.position);
+			Destroy(gameObject);
+		} else {
+			Vector2 normal = coll.contacts[0].normal;
+			if (Vector2.Angle(body.velocity, normal) < 45f){
+				if (rotator)
+					rotator.enabled = false;
+				if (body){
+					body.AddTorque((Random.Range(0, 2)*2-1) * Random.Range(2f, 4f), ForceMode2D.Force);
+					if (body.velocity.magnitude > 2f)
+						body.velocity = Vector2.ClampMagnitude(body.velocity, 2f);
+				}
+			}
+			if (coll.relativeVelocity.magnitude > 0.4f){
+				if (wallImpactSounds.Length > 0)
+					Toolbox.Instance.AudioSpeaker(wallImpactSounds[Random.Range(0, wallImpactSounds.Length)], transform.position);
+			}
+		}
+	}
+}
---
+++ locations/DeathTrap1.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+
+public class DeathTrap1 : MonoBehaviour {
+	public GameObject potion;
+	public GameObject door;
+	public Transform firePoint;
+	public GameObject dart;
+	public float timer;
+	public bool trapActive;
+	public bool trapSprung;
+	public AudioClip fireSound;
+	public AudioClip doorOpenSound;
+	public AudioClip tripSound;
+	public Pickup potionPickup;
+	public float fireAngle = 224f;
+	public int fireCycles;
+	public bool trapDone;
+	public CameraControl cameraControl;
+	public GameObject doorWay;
+	void Awake(){
+		doorWay.SetActive(false);
+		// trapSprung = true;
+		// trapActive = true;
+	}
+	void Update(){
+		timer += Time.deltaTime;
+		if (timer > 2 && !trapActive){
+			trapActive = true;
+			timer = 0;
+		}
+		if (!trapActive)
+			return;
+		if (potion == null && !trapSprung){
+			trapSprung = true;
+			Toolbox.Instance.AudioSpeaker(tripSound, door.transform.position);
+			cameraControl.Shake(0.2f);
+			timer = -3f;
+		}
+		if (potionPickup.holder != null && !trapSprung){
+			trapSprung = true;
+			Toolbox.Instance.AudioSpeaker(tripSound, potion.transform.position);
+			cameraControl.Shake(0.2f);
+			timer = -3f;
+		}
+		if (trapSprung && timer > 0.1f && !trapDone){
+			fireAngle -= 5f;
+			timer = 0;
+			GameObject dartObj = Instantiate(dart, firePoint.position, Quaternion.LookRotation(Vector2.left, Vector3.up));
+			Rigidbody2D dartBody = dartObj.GetComponent<Rigidbody2D>();
+			dartBody.velocity = new Vector2(Mathf.Cos(fireAngle * 2 * Mathf.PI / 360f), Mathf.Sin(fireAngle * 2 * Mathf.PI / 360f))*5f;
+			Toolbox.Instance.AudioSpeaker(fireSound, firePoint.position);
+			if (fireAngle <= 130f){
+				fireAngle = 224f;
+				timer = -1f;
+				fireCycles += 1;
+				if (fireCycles >= 3){
+					trapDone = true;
+					timer = 0;
+				}
+			}
+		}
+		if (timer > 2f && trapDone && door != null){
+			Toolbox.Instance.AudioSpeaker(doorOpenSound, firePoint.position);
+			Destroy(door);
+			doorWay.SetActive(true);
+		}
+	}
+}
