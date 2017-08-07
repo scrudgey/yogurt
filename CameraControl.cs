e97eda1f99c2d0cc399ca88257f87cefcfde091d
RSHAW <scrudgey@gmail.com>
2017-08-07 14:37:45 -0700

    linting and some changes to how camera shake is applied

--- CameraControl.cs
+++ CameraControl.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 
 public class CameraControl : MonoBehaviour {
-	
 	public GameObject focus;
 	private Vector3 focusLastPosition = Vector3.zero;
 	private Vector3 focusVelocity = Vector3.zero;
@@ -11,80 +10,63 @@ public class CameraControl : MonoBehaviour {
 	private Vector3 shakeVector = Vector3.zero;
 	public float maxSize;
 	public float minSize;
-
 	public Vector2 maxXY;
 	public Vector2 minXY;
-
 	private Camera mainCamera;
-
-	// Use this for initialization
 	void Start () {
 		mainCamera = GetComponent<Camera>();
 	}
-
 	public void Shake(float intensity){
 		StartCoroutine(screenShake(intensity));
 	}
-
 	private IEnumerator screenShake(float intensity){
-
 		while(intensity > 0.01){
 			shakeVector = Random.insideUnitCircle * intensity;
-			intensity = intensity * 0.90f;
+			intensity = intensity * 0.95f;
 			yield return null;
 		}
 	}
-
-	// Update is called once per frame
 	void FixedUpdate () {
-
-		if (focus){
-
-			Vector3 lowerLeftWorld;
-			Vector3 upperRightWorld;
-			Vector3 tempVector;
-			float screenWidthWorld;
-			float screenHeightWorld;
-
-			tempVector = transform.position;
-			// interpolate target velocity
-			if (focusLastPosition != Vector3.zero){
-				focusVelocity = (focus.transform.position-focusLastPosition)/Time.deltaTime;
-			} 
-
-			// update camera zoom
-			mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, minSize + focus.GetComponent<Rigidbody2D>().velocity.magnitude/8f , 0.05f);
-			if (mainCamera.orthographicSize > maxSize) { mainCamera.orthographicSize = maxSize;}
-
-			// update camera world coordinates.
-			lowerLeftWorld = GetComponent<Camera>().ScreenToWorldPoint( Vector2.zero);
-			upperRightWorld = GetComponent<Camera>().ScreenToWorldPoint ( new Vector2(GetComponent<Camera>().pixelWidth,GetComponent<Camera>().pixelHeight) );
-			screenWidthWorld = upperRightWorld.x - lowerLeftWorld.x;
-			screenHeightWorld = upperRightWorld.y - lowerLeftWorld.y;
-
-			tempVector = Vector3.SmoothDamp(transform.position , focus.transform.position + focusVelocity* focusLead,ref smoothVelocity,0.1f);
-			tempVector = tempVector + shakeVector;
-
-			//check for edge of level
-			if (tempVector.x - screenWidthWorld/2 < minXY.x){
-				tempVector.x = minXY.x + screenWidthWorld/2;
-			}
-			if (tempVector.y - screenHeightWorld/2 < minXY.y){
-				tempVector.y = minXY.y + screenHeightWorld/2;
-			}
-			if (tempVector.x + screenWidthWorld/2 > maxXY.x){
-				tempVector.x = maxXY.x - screenWidthWorld/2;
-			}
-			if (tempVector.y + screenHeightWorld/2 > maxXY.y){
-				tempVector.y = maxXY.y - screenHeightWorld/2;
-			}
-
-			// update camera position
-			tempVector.z = -1f;
-			transform.position = tempVector;
-			focusLastPosition = focus.transform.position;
+		if (focus == null)
+			return;
+		Vector3 lowerLeftWorld;
+		Vector3 upperRightWorld;
+		Vector3 tempVector;
+		float screenWidthWorld;
+		float screenHeightWorld;
+		tempVector = transform.position;
+		// interpolate target velocity
+		if (focusLastPosition != Vector3.zero){
+			focusVelocity = (focus.transform.position-focusLastPosition)/Time.deltaTime;
+		} 
+		// update camera zoom
+		mainCamera.orthographicSize = Mathf.Lerp(mainCamera.orthographicSize, minSize + focus.GetComponent<Rigidbody2D>().velocity.magnitude/8f, 0.05f);
+		if (mainCamera.orthographicSize > maxSize) {
+			mainCamera.orthographicSize = maxSize;
 		}
-						
+		// update camera world coordinates.
+		lowerLeftWorld = GetComponent<Camera>().ScreenToWorldPoint( Vector2.zero);
+		upperRightWorld = GetComponent<Camera>().ScreenToWorldPoint ( new Vector2(GetComponent<Camera>().pixelWidth,GetComponent<Camera>().pixelHeight) );
+		screenWidthWorld = upperRightWorld.x - lowerLeftWorld.x;
+		screenHeightWorld = upperRightWorld.y - lowerLeftWorld.y;
+		tempVector = Vector3.SmoothDamp(transform.position , focus.transform.position + focusVelocity* focusLead,ref smoothVelocity, 0.1f);
+		//check for edge of level
+		if (tempVector.x - screenWidthWorld/2 < minXY.x){
+			tempVector.x = minXY.x + screenWidthWorld/2;
+		}
+		if (tempVector.x + screenWidthWorld/2 > maxXY.x){
+			tempVector.x = maxXY.x - screenWidthWorld/2;
+		}
+		if (tempVector.y - screenHeightWorld/2 < minXY.y){
+			tempVector.y = minXY.y + screenHeightWorld/2;
+		}
+		if (tempVector.y + screenHeightWorld/2 > maxXY.y){
+			tempVector.y = maxXY.y - screenHeightWorld/2;
+		}
+		tempVector = tempVector + shakeVector;
+		// update camera position
+		tempVector.z = -1f;
+		transform.position = tempVector;
+		focusLastPosition = focus.transform.position;
 	}
-	
 }
