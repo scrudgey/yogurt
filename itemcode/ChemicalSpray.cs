f507084df3cf6933ec3f7db921d0118749adcd97
RSHAW <scrudgey@gmail.com>
2017-08-12 15:33:22 -0700

    linting

--- itemcode/ChemicalSpray.cs
+++ itemcode/ChemicalSpray.cs
@@ -3,14 +3,13 @@ public class ChemicalSpray: MonoBehaviour {
 	public GameObject collisionPlume;
 	private float height;
 	void OnCollisionEnter2D(Collision2D coll){
-		Instantiate(collisionPlume,transform.position,Quaternion.identity);
+		Instantiate(collisionPlume, transform.position,Quaternion.identity);
 		Destroy(gameObject);
 		Flammable flam = coll.gameObject.GetComponentInChildren<Flammable>();
 		if (flam){
 			if (flam.onFire){
 				flam.onFire = false;
 				flam.heat = -10f;
-
 				OccurrenceFire fireData = new OccurrenceFire();
 				fireData.objectName = Toolbox.Instance.CloneRemover(coll.gameObject.name);
 				fireData.extinguished = true;
