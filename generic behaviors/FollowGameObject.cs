edd77bafc00f6477a1e5b4508277ee4baaa87f56
RSHAW <scrudgey@gmail.com>
2017-08-13 11:19:47 -0700

    initial commit

---
+++ generic behaviors/FollowGameObject.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class FollowGameObject : MonoBehaviour {
+	public GameObject target;
+	private Vector3 offset;
+	public void Init(){
+		if (target == null)
+			Destroy(this);
+		offset = transform.position - target.transform.position;
+	}
+	public void Update(){
+		if (target == null)
+			Destroy(this);
+		transform.position = target.transform.position + offset;
+	}
+}
