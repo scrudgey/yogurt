c1afca160c556c3066692bb9e6d984b9549c384c
RSHAW <scrudgey@gmail.com>
2017-08-23 22:07:37 -0700

    fixing fire propagation hierarchy issues

--- generics/Flammable.cs
+++ generics/Flammable.cs
@@ -4,6 +4,7 @@ public class Flammable : MonoBehaviour {
 	public float flashpoint;
 	public bool onFire;
 	public bool fireSource;
+	public bool noDamage;
 	public ParticleSystem smoke;
 	public ParticleSystem fireParticles;
 	private CircleCollider2D fireRadius;
@@ -13,7 +14,10 @@ public class Flammable : MonoBehaviour {
 	private float flagTimer;
 	public GameObject responsibleParty;
 	public bool playSounds = true;
+	public Pickup pickup;
 	void Start () {
+		pickup = GetComponent<Pickup>();
+
 		//ensure that there is a speaker
 		audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
 		burnSounds = Resources.Load("sounds/Crackling Fire", typeof(AudioClip)) as AudioClip;
@@ -86,6 +90,10 @@ public class Flammable : MonoBehaviour {
 			Toolbox.Instance.OccurenceFlag(gameObject, fireData);
 		}
 		if (onFire){
+			if (pickup){
+				if (pickup.holder != null)
+					responsibleParty = pickup.holder.gameObject;
+			}
 			flagTimer += Time.deltaTime;
 			if (flagTimer > 0.5f){
 				flagTimer = 0;
@@ -94,12 +102,12 @@ public class Flammable : MonoBehaviour {
 				fireData.chaos = 100;
 				Toolbox.Instance.OccurenceFlag(gameObject, fireData);
 			}
-			// TODO: make this more sophisticated,
+			// if i am on fire, i take damage.
 			MessageDamage message = new MessageDamage(Time.deltaTime, damageType.fire);
 			Toolbox.Instance.SendMessage(gameObject, this, message, sendUpwards: false);
 			if (Random.Range(0, 100f) < 1){
 				MessageSpeech speechMessage = new MessageSpeech();
-				speechMessage.phrase = "hot!";
+				speechMessage.phrase = "this "+gameObject.name+" is hot!";
 				Toolbox.Instance.SendMessage(gameObject, this, speechMessage, sendUpwards: true);
 			}
 		}
