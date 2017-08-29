43d90fe24999152760bb95eb90de21304b94647e
RSHAW <scrudgey@gmail.com>
2017-08-28 18:57:19 -0700

    addchild function added

--- savesystem/MyMarker.cs
+++ savesystem/MyMarker.cs
@@ -9,6 +9,10 @@ public class MyMarker : MonoBehaviour, IExcludable {
 	void OnEnable(){
 		MySaver.disabledPersistents.Remove(gameObject);
 	}
+	public void addChild(GameObject target){
+		ClaimsManager.Instance.ClaimObject(target, this);
+		persistentChildren.Add(target);
+	}
 	void OnDestroy(){
 		if (MySaver.disabledPersistents.Contains(gameObject)){
 			MySaver.disabledPersistents.Remove(gameObject);
