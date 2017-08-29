04a822d43f58b0dc3387246dafecd001f983eb4e
RSHAW <scrudgey@gmail.com>
2017-08-28 18:52:52 -0700

    stain has its own configuration method now

--- Stain.cs
+++ Stain.cs
@@ -3,9 +3,21 @@
 public class Stain : MonoBehaviour {
 	public SpriteMask parentMask;
 	public SpriteRenderer parentRenderer;
+	public GameObject parent;
 	void Update(){
 		if (parentRenderer != null){
 			parentMask.sprite = parentRenderer.sprite;
 		}
+	}
+	public void ConfigureParentObject(GameObject parent){
+		this.parent = parent;
+		parentMask = Toolbox.Instance.GetOrCreateComponent<SpriteMask>(parent);
+		parentRenderer = parent.GetComponent<SpriteRenderer>();
+		SpriteRenderer stainRenderer = GetComponent<SpriteRenderer>();
+		stainRenderer.maskInteraction = SpriteMaskInteraction.VisibleInsideMask;
+		transform.SetParent(parent.transform, true);
+	}
+	public void RemoveStain(){
+		Destroy(gameObject);
 	}
 }
