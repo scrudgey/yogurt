10178fb63e7919975a74a6c3ae20324d9e913414
RSHAW <scrudgey@gmail.com>
2017-10-17 10:12:15 -0700

    new damage callback system

--- itemcode/Slasher.cs
+++ itemcode/Slasher.cs
@@ -1,40 +1,21 @@
 ﻿using UnityEngine;
-// using System.Collections.Generic;
-// using System.Collections;
 
 public class Slasher : MonoBehaviour {
-	public AudioClip[] impactSounds;
-	public Vector2 direction; 
-	// public List<GameObject> owners = new List<GameObject>();
-	public GameObject responsibleParty;
-	public float damage;
-	
+	public MessageDamage message;
 	void SlashOn(){
 		Vector3 startPoint = transform.position;
-		startPoint.x += direction.x / 4f;
-		startPoint.y += direction.y / 4f;
-
+		startPoint.x += message.force.normalized.x / 4f;
+		startPoint.y += message.force.normalized.y / 4f;
 		GameObject impactObj = GameManager.Instantiate(Resources.Load("PhysicalImpact"), startPoint, Quaternion.identity) as GameObject;
 		PhysicalImpact impact = impactObj.GetComponent<PhysicalImpact>();
-		impact.impactSounds = impactSounds;
-		impact.direction = direction;
 		impact.size = 0.11f;
-		impact.magnitude = damage;
-		impact.responsibleParty = responsibleParty;
-
+		impact.message = message;
 		CircleCollider2D impactCollider = impact.GetComponent<CircleCollider2D>();
-		// foreach (GameObject owner in owners){
-			// Collider2D[] ownerColliders = responsibleParty.GetComponentsInChildren<Collider2D>();
-			// foreach (Collider2D ownerCollider in ownerColliders){
-				// Physics2D.IgnoreCollision(impactCollider, ownerCollider, true);
-		foreach (Collider2D responsibleCollider in responsibleParty.GetComponentsInChildren<Collider2D>())
+		foreach (Collider2D responsibleCollider in message.responsibleParty.GetComponentsInChildren<Collider2D>())
 			Physics2D.IgnoreCollision(impactCollider, responsibleCollider, true);
-			// }
-		// }
-		
 	}
 	void SlashOff(){
-		// GetComponent<Collider2D>().enabled = false;
+		// currently empty but referred to in animation
 	}
 	void SlashEnd(){
 		ClaimsManager.Instance.WasDestroyed(gameObject);
