8be3d7422487f7579242d8568f3842fcfb23118e
RSHAW <scrudgey@gmail.com>
2017-10-17 09:53:50 -0700

    melee weapons have damage type

--- generics/MeleeWeapon.cs
+++ generics/MeleeWeapon.cs
@@ -1,10 +1,7 @@
 ﻿using UnityEngine;
-// using System.Collections;
-
 public class MeleeWeapon : Pickup {
-
 	public float damage;
 	public AudioClip[] impactSounds;
 	public AudioClip[] swingSounds;
-
+	public damageType damageType;
 }
