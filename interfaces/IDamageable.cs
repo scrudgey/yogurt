74cc8c7bc0e4f3ac164609b49b2010fa9cca9684
RSHAW <scrudgey@gmail.com>
2017-10-21 11:24:33 -0700

    major overhaul to damageable

--- interfaces/IDamageable.cs
+++ interfaces/IDamageable.cs
@@ -1,5 +1,44 @@
-public enum ImpactResult {normal, repel, strong}
+using UnityEngine;
+using System.Collections;
 
-public interface IDamageable {
-    ImpactResult TakeDamage(MessageDamage message);
+public enum ImpactResult {normal, repel, strong}
+public class Damageable: MonoBehaviour, IMessagable{
+    public damageType lastDamage;
+    public GameObject gibsContainerPrefab;
+    public ImpactResult TakeDamage(MessageDamage message){
+        lastDamage = message.type;
+        return CalculateDamage(message);
+    }
+    public virtual ImpactResult CalculateDamage(MessageDamage message){
+        return ImpactResult.normal;
+    }
+    public virtual void Start(){
+        if (gibsContainerPrefab != null){
+            GameObject gibsContainer = Instantiate(gibsContainerPrefab) as GameObject;
+            foreach(Gibs gib in gibsContainer.GetComponents<Gibs>()){
+                Gibs newGib = gameObject.AddComponent<Gibs>();
+                newGib.CopyFrom(gib);
+            }
+            Destroy(gibsContainer);
+        }
+    }
+    public virtual void Destruct(){
+        foreach (Gibs gib in GetComponents<Gibs>())
+            gib.Emit(lastDamage);
+        PhysicalBootstrapper phys = GetComponent<PhysicalBootstrapper>();
+		if (phys){
+			phys.DestroyPhysical();
+		}
+        ClaimsManager.Instance.WasDestroyed(gameObject);
+        Destroy(gameObject);
+    }
+    public virtual void ReceiveMessage(Message message){
+		if (message is MessageDamage){
+			MessageDamage dam = (MessageDamage)message;
+			ImpactResult result = TakeDamage(dam);
+            if (dam.impactor){
+			    dam.impactor.PlayImpactSound(result);
+		    }
+        }
+	}
 }
