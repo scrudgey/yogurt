ed74d144e3cdd051ece1a4b91d2ec1df1175b72f
RSHAW <scrudgey@gmail.com>
2017-10-21 11:18:49 -0700

    create stain when colliding with main layer

--- MonoLiquid.cs
+++ MonoLiquid.cs
@@ -3,6 +3,7 @@ public class MonoLiquid : MonoBehaviour {
 	public Liquid liquid;
 	public Edible edible;
 	public PhysicalBootstrapper physB;
+	public bool ignoreCollisions;
 	static GameObject CreateStain(GameObject target, Vector3 location){
 		GameObject stainObject = Instantiate(Resources.Load("prefabs/stain")) as GameObject;
 		stainObject.transform.position = location;
@@ -24,24 +25,27 @@ public class MonoLiquid : MonoBehaviour {
 	}
 	//TODO: figure out why splashes are happening instead of puddles for vomit
 	void OnCollisionEnter2D(Collision2D coll){
+		if (ignoreCollisions)
+			return;
 		if (physB == null)
 			return;
 		if (physB.physical == null)
 			return;
-		if (physB.physical.currentMode == Physical.mode.fly){
-			GameObject splash = Instantiate(Resources.Load("prefabs/splash"), transform.position, Quaternion.identity) as GameObject;
-			SpriteRenderer splashSprite = splash.GetComponent<SpriteRenderer>();
-			splashSprite.color = liquid.color;
-			Transform rectTransform = splash.GetComponent<Transform>();
- 			rectTransform.Rotate(new Vector3(0, 0, Random.Range(0f, 360f)));
+		if (coll.collider.gameObject.layer == LayerMask.NameToLayer("main"))
+			if (physB.physical.currentMode == Physical.mode.fly){
+				GameObject splash = Instantiate(Resources.Load("prefabs/splash"), transform.position, Quaternion.identity) as GameObject;
+				SpriteRenderer splashSprite = splash.GetComponent<SpriteRenderer>();
+				splashSprite.color = liquid.color;
+				Transform rectTransform = splash.GetComponent<Transform>();
+				rectTransform.Rotate(new Vector3(0, 0, Random.Range(0f, 360f)));
 
-			GameObject stain = CreateStain(coll.gameObject, transform.position);
-			SpriteRenderer stainRenderer = stain.GetComponent<SpriteRenderer>();
-			stainRenderer.color = liquid.color;
-			Liquid.MonoLiquidify(stain, liquid);
-			ClaimsManager.Instance.WasDestroyed(gameObject);
-			Destroy(gameObject);
-		}
+				GameObject stain = CreateStain(coll.gameObject, transform.position);
+				SpriteRenderer stainRenderer = stain.GetComponent<SpriteRenderer>();
+				stainRenderer.color = liquid.color;
+				Liquid.MonoLiquidify(stain, liquid);
+				ClaimsManager.Instance.WasDestroyed(gameObject);
+				Destroy(gameObject);
+			}
 	}
 	public void LoadLiquid(string type){
 		// TODO: make this fancier: retrieve liquid in buffer, only change variable
@@ -56,7 +60,12 @@ public class MonoLiquid : MonoBehaviour {
 		PhysicalBootstrapper pb = GetComponent<PhysicalBootstrapper>();
 		pb.DestroyPhysical();
 		Liquid.MonoLiquidify(puddle, liquid);
-		puddle.GetComponent<Edible>().offal = true;
+		Edible puddleEdible = puddle.GetComponent<Edible>();
+		Edible edible = GetComponent<Edible>();
+		if (edible){
+			puddleEdible.human = edible.human;
+			puddleEdible.offal = true;
+		}
 		Destroy(gameObject);
 		ClaimsManager.Instance.WasDestroyed(gameObject);
 	}
