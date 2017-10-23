62967d85540894d2df03edb36d26ebbd448b6ffd
RSHAW <scrudgey@gmail.com>
2017-10-23 09:17:50 -0700

    gibs have the damage matching logic.

--- generics/Gibs.cs
+++ generics/Gibs.cs
@@ -1,5 +1,4 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public class Gibs : MonoBehaviour {
 	public damageType damageCondition;
@@ -9,10 +8,13 @@ public class Gibs : MonoBehaviour {
 	public float forceMax;
 	public Color color = Color.white;
 	public void Emit(damageType dam){
-		// if (dam = damageCondition)
+		if (!DamageTypeMatch(damageCondition, dam))
+			return;
 		for (int i =0; i<number; i++){
 			GameObject bit = Instantiate(particle) as GameObject;
-			bit.transform.position = transform.position;
+			Vector3 randomWalk = 0.1f * Random.insideUnitCircle.normalized;
+			randomWalk.z = 0;
+			bit.transform.position = transform.position + randomWalk;
 			Rigidbody2D bitPhys = bit.GetComponent<Rigidbody2D>();
 			if (bitPhys){
 				Vector2 force = Random.insideUnitCircle;
@@ -23,10 +25,20 @@ public class Gibs : MonoBehaviour {
 			sprite.color = color;
 			Damageable damageable = bit.GetComponent<Damageable>();
 			if (damageable){
-				// StartCoroutine(damageable.EnableAfterSeconds(2f));
-				Toolbox.Instance.DisableAndReenable(damageable, 0.1f);
+				Toolbox.Instance.DisableAndReenable(damageable, 0.2f);
 			}
 		}
+	}
+	public static bool DamageTypeMatch(damageType dam1, damageType dam2){
+		if (dam1 == dam2)
+			return true;
+		if (dam1 == damageType.any || dam2 == damageType.any)
+			return true;
+		if (dam1 == damageType.physical && (dam2 == damageType.cutting || dam2 == damageType.physical || dam2 == damageType.piercing))
+			return true;
+		if (dam2 == damageType.physical && (dam1 == damageType.cutting || dam1 == damageType.physical || dam1 == damageType.piercing))
+			return true;
+		return false;
 	}
 	public void CopyFrom(Gibs other){
 		damageCondition = other.damageCondition;
