17ff2d0a164058bdfd67cff8afe3d9aeba1368c2
RSHAW <scrudgey@gmail.com>
2017-12-24 22:44:33 -0800

    initial commit

---
+++ AI/PeterPicklebottom.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using AI;
+
+public class PeterPicklebottom : MonoBehaviour {
+    public enum AIState{none, walkToTarget, slewAtTarget, leave}
+    public AIState state;
+    public Controllable controllable;
+    public Ref<GameObject> target;
+    public float timer;
+    public Stack<Pickup> targets;
+    private RoutineWalkToGameobject routine;
+    public Doorway door;
+    public AudioClip theme;
+    public AudioClip[] takeSound;
+    private AudioSource audioSource;
+    public void Start(){
+        controllable = GetComponent<Controllable>();
+        target = new Ref<GameObject>(null);
+        if (targets == null){
+            targets = new Stack<Pickup>();
+             foreach(Pickup pickup in FindObjectsOfType<Pickup>()){
+                targets.Push(pickup);
+            }
+        }
+        if (targets.Count > 0){
+            target.val = targets.Pop().gameObject;
+        }
+        state = AIState.walkToTarget;
+        routine = new RoutineWalkToGameobject(gameObject, controllable, target);
+        routine.minDistance = 0.1f;
+        timer = -2f;
+        audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
+    }
+    public void PlayThemeSong(){
+        audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
+        audioSource.PlayOneShot(theme);
+    }
+    public void Update(){
+        timer += Time.deltaTime;
+        if (timer < 0)
+            return;
+        if (target.val == null && targets.Count == 0 && state != AIState.leave){
+            state = AIState.leave;
+        }
+        switch(state){
+            case AIState.walkToTarget:
+            float distanceToTarget = Vector2.Distance(transform.position, target.val.transform.position);
+            if (distanceToTarget > 0.1f){
+                routine.Update();
+            } else {
+                timer = 0;
+                state = AIState.slewAtTarget;
+                controllable.ResetInput();
+            }
+            break;
+            case AIState.slewAtTarget:
+            if (timer > 1f && target.val != null){
+                Destroy(target.val);
+                audioSource.PlayOneShot(takeSound[Random.Range(0, takeSound.Length)]);
+            }
+            if (timer > 2f){
+                state = AIState.walkToTarget;
+                target.val = targets.Pop().gameObject;
+                timer = 0f;
+            }
+            break;
+            case AIState.leave:
+            target.val = door.gameObject;
+            float doorDistance = Vector2.Distance(transform.position, target.val.transform.position);
+            if (doorDistance > 0.1f){
+                routine.Update();
+            } else {
+                door.PlayExitSound();
+                Destroy(gameObject);
+            }
+            break;
+            default:
+            break;
+        }
+    }
+}
---
+++ UI/UIStatusIcon.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIStatusIcon : MonoBehaviour {
+	public Buff buff;
+	public RectTransform lifeBar;
+	public Text text;
+	private Vector2 lifebarDefaultSize;
+	public void Initialize(BuffType type, Buff buff){
+		this.buff = buff;
+		text = transform.Find("Text").GetComponent<Text>();
+		lifeBar = transform.Find("lifebar/mask/fill").GetComponent<RectTransform>();
+
+		text.text = type.ToString();
+		if (lifebarDefaultSize == Vector2.zero)
+			lifebarDefaultSize = new Vector2(lifeBar.rect.width, lifeBar.rect.height);
+		if (buff.lifetime == 0){
+			transform.Find("lifebar").gameObject.SetActive(false);
+		}
+	}
+	public void Update(){
+		if (buff == null)
+			Destroy(gameObject);
+		if (buff.lifetime > 0){
+			float width = (1 - (buff.time / buff.lifetime)) * lifebarDefaultSize.x;
+			lifeBar.sizeDelta = new Vector2(width, lifebarDefaultSize.y);
+		}
+	}
+}
