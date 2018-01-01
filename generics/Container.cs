6cc1d48af5e639ad06f7d4913048f567f72e17a4
RSHAW <scrudgey@gmail.com>
2017-12-31 17:49:58 -0800

    linting

--- generics/Container.cs
+++ generics/Container.cs
@@ -31,8 +31,6 @@ public class Container : Interactive, IExcludable, ISaveable {
 			}
 		}
 	}
-	
-
 	protected void RemoveRetrieveAction(Pickup pickup){
 		Interaction removeThis = null;
 		foreach (Interaction interaction in interactions)
