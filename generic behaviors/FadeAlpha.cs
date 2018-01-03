ee284c17db7d4c9bb4e82369111d0776e4e43de8
RSHAW <scrudgey@gmail.com>
2018-01-02 18:24:04 -0800

    initial commit

---
+++ generic behaviors/FadeAlpha.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class FadeAlpha : MonoBehaviour {
+	public HashSet<SpriteRenderer> spriteRenderers = new HashSet<SpriteRenderer>();
+	public float period = 3f;
+	public float minAlpha = 0.75f;
+	public float maxAlpha = 1f;
+	public float timer;
+	public void Start(){
+		spriteRenderers.Add(GetComponent<SpriteRenderer>());
+	}
+	public void Update(){
+		timer += Time.deltaTime;
+		foreach(SpriteRenderer spriteRenderer in spriteRenderers){
+			Color color = spriteRenderer.color;
+			color.a = minAlpha + (maxAlpha - minAlpha) * Mathf.Sin(timer * 6.28f / period);
+			spriteRenderer.color = color;
+		}
+	}
+}
