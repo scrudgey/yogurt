7c28a6315b5ffdc757dfb7ba07cbff67c6f55514
RSHAW <scrudgey@gmail.com>
2018-01-04 10:01:19 -0800

    initial commit

---
+++ UI/PerkButton.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PerkButton : MonoBehaviour {
+	public PerkMenu menu;
+	public Perk perk;
+	// public string perkTitle;
+	// public string perkDesc;
+	// public string perkName;
+	// public bool enabled;
+	public void Clicked(){
+		menu.PerkButtonClicked(this);
+	}
+	// public void MouseOver(){
+	// 	menu.PerkButtonMouseOver(this);
+	// }
+}
---
+++ UI/PerkMenu.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+public class PerkMenu : MonoBehaviour {
+	public Transform buttonList;
+	public Image perkImage;
+	public Text perkTitleText;
+	public Text perkDescText;
+	public Button doneButton;
+	public Button acceptButton;
+	public bool perkChosen;
+	public PerkButton selectedPerk;
+	void Start(){
+		GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
+		perkImage = transform.Find("menu/body/infoPanel/imagePanel/Image").GetComponent<Image>();
+
+		perkDescText = transform.Find("menu/body/infoPanel/textPanel/desc").GetComponent<Text>();
+		perkTitleText = transform.Find("menu/body/infoPanel/textPanel/title").GetComponent<Text>();
+		doneButton = transform.Find("menu/body/infoPanel/Done").GetComponent<Button>();
+		acceptButton = transform.Find("menu/body/infoPanel/Accept").GetComponent<Button>();
+
+		buttonList = transform.Find("menu/body/list");
+		doneButton.interactable = false;
+		acceptButton.interactable = false;
+
+		PopulatePerkList();
+	}
+	void PopulatePerkList(){
+		GameObject[] perkPrefabs = Resources.LoadAll("perks/", typeof(GameObject))
+			.Cast<GameObject>()
+			.ToArray();
+		foreach (GameObject prefab in perkPrefabs){
+			PerkComponent component = prefab.GetComponent<PerkComponent>();
+			if (component){
+				GameObject buttonObject = Instantiate(Resources.Load("UI/PerkButton")) as GameObject;
+				Text buttonText = buttonObject.transform.Find("Text").GetComponent<Text>();
+				buttonObject.transform.SetParent(buttonList, false);
+				PerkButton perkScript = buttonObject.GetComponent<PerkButton>();
+				perkScript.menu = this;
+				perkScript.perk = new Perk(component.perk);
+				buttonText.text = component.perk.title;
+				if (GameManager.Instance.data.perks[component.perk.name]){
+					Button perkButton = buttonObject.GetComponent<Button>();
+					perkButton.interactable = false; 
+				}
+			}
+		}
+	}
+	public void PerkButtonClicked(PerkButton buttonScript){
+		selectedPerk = buttonScript;
+		perkDescText.text = buttonScript.perk.desc;
+		perkTitleText.text = buttonScript.perk.title;
+		// change image
+		perkImage.sprite = buttonScript.perk.perkImage;
+		// perkImage.color = new Color(1f, 1f, 1f, 0f);
+
+		if (!doneButton.interactable)
+			acceptButton.interactable = true;
+	}
+	public void AcceptButtonClicked(){
+		if (selectedPerk == null)
+			return;
+		acceptButton.interactable = false;
+		doneButton.interactable = true;
+		GameManager.Instance.data.perks[selectedPerk.perk.name] = true;
+		selectedPerk.GetComponent<Button>().interactable = false;
+		UINew.Instance.UpdateButtons();
+		// play sfx?
+		UINew.Instance.PlayUISound("sounds/8-bit/BOUNCE3");
+	}
+	public void DoneButtonClick(){
+		UINew.Instance.CloseActiveMenu();
+	}
+}
