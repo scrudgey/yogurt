c1e2d26b8ab2c414e5890af2f6e4a3777f92c693
RSHAW <scrudgey@gmail.com>
2018-01-04 10:01:28 -0800

    initial commit

---
+++ itemcode/Chela.cs
@@ -0,0 +1,16 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+public class Chela : Item {
+	public void Start(){
+		Interaction perkAction = new Interaction(this, "Choose Perk...", "OpenPerkMenu");
+		interactions.Add(perkAction);
+	}
+	public void OpenPerkMenu(){
+		UINew.Instance.ShowMenu(UINew.MenuType.perk);
+	}
+	public string OpenPerkMenu_desc(){
+		return "Choose Perk";
+	}
+}
