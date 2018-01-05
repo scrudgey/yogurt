f6fb75297d79203047d9e7f8a285cfa78d7871de
RSHAW <scrudgey@gmail.com>
2018-01-05 15:59:07 -0800

    linting

--- UI/PerkButton.cs
+++ UI/PerkButton.cs
@@ -3,14 +3,7 @@
 public class PerkButton : MonoBehaviour {
 	public PerkMenu menu;
 	public Perk perk;
-	// public string perkTitle;
-	// public string perkDesc;
-	// public string perkName;
-	// public bool enabled;
 	public void Clicked(){
 		menu.PerkButtonClicked(this);
 	}
-	// public void MouseOver(){
-	// 	menu.PerkButtonMouseOver(this);
-	// }
 }
