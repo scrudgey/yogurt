a6ca7c6ef1b578f61dd930bec466862cfb51bd21
RSHAW <scrudgey@gmail.com>
2018-01-11 11:58:26 -0800

    moved nullification to duplicatabale

--- itemcode/CosmicNullifier.cs
+++ itemcode/CosmicNullifier.cs
@@ -8,35 +8,29 @@ public class CosmicNullifier : Pickup {
 	void Start(){
 		nullify = new Interaction(this, "Nullify", "Nullify");
 		nullify.limitless = true;
-		nullify.duplicator = true;
 		nullify.validationFunction = true;
-		nullify.inertOnPlayerConsent = false;
-		nullify.otherOnPlayerConsent = false;
+		// nullify.inertOnPlayerConsent = false;
+		// nullify.otherOnPlayerConsent = false;
 		interactions.Add(nullify);
 		audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
 	}
-	public void Nullify(){
-		if (nullifySound.Length > 0){
-			Toolbox.Instance.AudioSpeaker(nullifySound[Random.Range(0, nullifySound.Length)], nullify.duplicationTarget.transform.position);
-		}
-		if (nullifyParticleEffect != null){
-			Instantiate(nullifyParticleEffect, nullify.duplicationTarget.transform.position, Quaternion.identity);
-		}
-		Destroy(nullify.duplicationTarget);
-		ClaimsManager.Instance.WasDestroyed(gameObject);
+	public void Nullify(Duplicatable duplicatable){
+		if (duplicatable == null)
+			return;
+		duplicatable.Nullify();
 		Destroy(gameObject);
+		ClaimsManager.Instance.WasDestroyed(gameObject);
 	}
-	public bool Nullify_Validation(){
-		GameObject target = nullify.duplicationTarget;
-		if (target == null)
+	public bool Nullify_Validation(Duplicatable duplicatable){
+		if (duplicatable.gameObject == null)
 			return false;
-		if (target.gameObject == gameObject){
+		if (duplicatable.gameObject == gameObject){
 			return false;
 		} else {
-			return true;
+			return duplicatable.Nullifiable();
 		}
 	}
-	public string Nullify_desc(){
-		return "Nullify "+Toolbox.Instance.GetName(nullify.duplicationTarget);
+	public string Nullify_desc(Duplicatable duplicatable){
+		return "Nullify "+Toolbox.Instance.GetName(duplicatable.gameObject);
 	}
 }
