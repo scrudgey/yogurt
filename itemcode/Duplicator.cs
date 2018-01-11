4fc1ee85ba9b3293fee921d174414d21d26dbd52
RSHAW <scrudgey@gmail.com>
2018-01-11 11:59:23 -0800

    simplified duplication with duplicatable component

--- itemcode/Duplicator.cs
+++ itemcode/Duplicator.cs
@@ -5,53 +5,46 @@ public class Duplicator : Interactive, IDirectable {
 	public AudioClip failSound;
 	private AudioSource audioSource;
 	public ParticleSystem particles;
-	public Interaction dup;
 	public void DirectionChange(Vector2 d){
 		if (particles){
 			particles.transform.rotation = Quaternion.AngleAxis(Toolbox.Instance.ProperAngle(d.x, d.y)-20f, new Vector3(0, 0, 1));
 		}
 	}
-	void Start() {
+	void Start(){
 		audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
-		dup = new Interaction(this, "Duplicate", "Duplicate");
+		Interaction dup = new Interaction(this, "Duplicate", "Duplicate");
 		dup.limitless = true;
-		dup.duplicator = true;
 		dup.validationFunction = true;
-		dup.inertOnPlayerConsent = false;
-		dup.otherOnPlayerConsent = false;
+		// dup.inertOnPlayerConsent = false;
+		// dup.otherOnPlayerConsent = false;
 		interactions.Add(dup);
 	}
-	public void Duplicate(){
-		GameObject target = dup.duplicationTarget;
-		string prefabName = Toolbox.Instance.CloneRemover(target.name);
+	public void Duplicate(Duplicatable duplicatable){
 		Vector3 jitter = new Vector3(Random.Range(0, 0.1f), Random.Range(0, 0.1f), 0);
-		GameObject dupObj = Instantiate(Resources.Load("prefabs/"+prefabName), target.transform.position + jitter, Quaternion.identity) as GameObject;
-		if (dup == null){
-			audioSource.PlayOneShot(failSound);
-		} else {
-			dupObj.name = Toolbox.Instance.CloneRemover(dupObj.name);
-			audioSource.PlayOneShot(dupSound);
-			Instantiate(Resources.Load("particles/poof"), dupObj.transform.position, Quaternion.identity);
-			if (prefabName == "dollar"){
-				GameManager.Instance.data.achievementStats.dollarsDuplicated += 1;
-				GameManager.Instance.CheckAchievements();
-			}
-			if (particles != null){
-				particles.Play();
-			}
+		GameObject dupObj = Instantiate(duplicatable.duplicationPrefab, duplicatable.transform.position + jitter, Quaternion.identity) as GameObject;
+		dupObj.name = Toolbox.Instance.CloneRemover(dupObj.name);
+		audioSource.PlayOneShot(dupSound);
+		Instantiate(Resources.Load("particles/poof"), dupObj.transform.position, Quaternion.identity);
+		if (duplicatable.duplicationPrefab.name == "dollar"){
+			GameManager.Instance.data.achievementStats.dollarsDuplicated += 1;
+			GameManager.Instance.CheckAchievements();
+		}
+		if (particles != null){
+			particles.Play();
 		}
 	}
-	public bool Duplicate_Validation(){
-		GameObject target = dup.duplicationTarget;
-		if (target == null)
+	public bool Duplicate_Validation(Duplicatable duplicatable){
+		if (!duplicatable.duplicatable)
 			return false;
-		if (target.gameObject == gameObject){
+		if (duplicatable.duplicationPrefab == null)
+			return false;
+		if (duplicatable.gameObject == gameObject){
 			return false;
 		} else {
 			return true;
 		}
 	}
-	public string Duplicate_desc(){
-		return "Duplicate "+Toolbox.Instance.GetName(dup.duplicationTarget);
+	public string Duplicate_desc(Duplicatable duplicatable){
+		return "Duplicate "+Toolbox.Instance.GetName(duplicatable.duplicationPrefab);
 	}
 }
