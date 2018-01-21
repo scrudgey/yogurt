af9bddf7160c82a02e0c49764022137452098adb
RSHAW <scrudgey@gmail.com>
2018-01-20 22:52:35 -0800

    damage zone responsible party is not the victim

--- generic behaviors/DamageZone.cs
+++ generic behaviors/DamageZone.cs
@@ -23,7 +23,7 @@ public class DamageZone : MonoBehaviour {
 			timerVal -= Time.deltaTime;
 			if (timerVal <= 0){
 				timerVal = timerInterval;
-				message.responsibleParty = other.transform.root.gameObject;
+				message.responsibleParty = gameObject;
 				Toolbox.Instance.SendMessage(other.gameObject, this, message, sendUpwards: true);
 			}
 			hitTimeouts[other.gameObject] = timerVal;
