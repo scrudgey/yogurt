72327c9e608d93b2005c0215cfd10c6b3c08bdcd
RSHAW <scrudgey@gmail.com>
2018-03-06 16:26:00 -0800

    test of code formatting / linting automatically

--- Email.cs
+++ Email.cs
@@ -1,38 +1,37 @@
 ﻿using System;
-using UnityEngine;
-using System.Xml.Serialization;
 using System.IO;
+using System.Xml.Serialization;
+using UnityEngine;
 
 [System.Serializable]
 public class Email {
-	public string filename;
-	public string fromString;
-	public string toString;
-	public string subject;
-	public string content;
-	public bool read;
+    public string filename;
+    public string fromString;
+    public string toString;
+    public string subject;
+    public string content;
+    public bool read;
 
-	public static Email LoadEmail(string filename){
-		try{
+    public static Email LoadEmail(string filename) {
+        try {
             Email newEmail = null;
-            TextAsset xml = Resources.Load("data/emails/"+filename) as TextAsset;
+            TextAsset xml = Resources.Load("data/emails/" + filename) as TextAsset;
             var serializer = new XmlSerializer(typeof(Email));
             var reader = new System.IO.StringReader(xml.text);
             newEmail = serializer.Deserialize(reader) as Email;
-			newEmail.filename = filename;
+            newEmail.filename = filename;
             return newEmail;
-        } catch(Exception e){
+        } catch (Exception e) {
             Debug.Log(e.Message);
             return null;
         }
-	}
-
-	public static void SaveEmail(Email email){
-		var serializer = new XmlSerializer(typeof(Email));
-		string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
-		path = Path.Combine(path, "email.xml");
-		FileStream sceneStream = File.Create(path);
-		serializer.Serialize(sceneStream, email);
-		sceneStream.Close();
-	}
+    }
+    public static void SaveEmail(Email email) {
+        var serializer = new XmlSerializer(typeof(Email));
+        string path = Path.Combine(Application.persistentDataPath, GameManager.Instance.saveGameName);
+        path = Path.Combine(path, "email.xml");
+        FileStream sceneStream = File.Create(path);
+        serializer.Serialize(sceneStream, email);
+        sceneStream.Close();
+    }
 }
