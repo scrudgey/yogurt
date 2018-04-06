2f304fea5869349ef0dcb891b1597434cf9cc15c
RSHAW <scrudgey@gmail.com>
2018-04-06 09:23:45 -0700

    support for 3D gibs

--- generics/Gibs.cs
+++ generics/Gibs.cs
@@ -5,16 +5,27 @@ public class Gibs : MonoBehaviour {
     public int number;
     public GameObject particle;
     public Color color = Color.white;
+    public bool notPhysical;
     public void Emit(damageType dam, Vector2 baseDir) {
         if (!DamageTypeMatch(damageCondition, dam))
             return;
         for (int i = 0; i < number; i++) {
-            GameObject bit = Instantiate(particle) as GameObject;
+            GameObject bit = Instantiate(particle, transform.position, Quaternion.identity) as GameObject;
+            SpriteRenderer sprite = bit.GetComponent<SpriteRenderer>();
+            sprite.color = color;
+            Damageable damageable = bit.GetComponent<Damageable>();
+            if (damageable) {
+                Toolbox.Instance.DisableAndReenable(damageable, 0.2f);
+            }
+            if (notPhysical)
+                return;
             Rigidbody2D bitBody = Toolbox.Instance.GetOrCreateComponent<Rigidbody2D>(bit);
             PhysicalBootstrapper bitPhys = Toolbox.Instance.GetOrCreateComponent<PhysicalBootstrapper>(bit);
             PhysicalBootstrapper myBoot = GetComponent<PhysicalBootstrapper>();
             bitPhys.impactsMiss = true;
-            bitPhys.initHeight = Random.Range(0.01f, 0.075f);
+            if (bitPhys.size == PhysicalBootstrapper.shadowSize.normal)
+                bitPhys.size = PhysicalBootstrapper.shadowSize.medium;
+            bitPhys.initHeight = Random.Range(0.025f, 0.075f);
             if (myBoot) {
                 if (myBoot.physical != null) {
                     bitPhys.initHeight = myBoot.physical.height;
@@ -23,17 +34,10 @@ public class Gibs : MonoBehaviour {
             Vector3 randomWalk = 0.05f * Random.insideUnitCircle.normalized;
             randomWalk.z = 0;
             bit.transform.position = transform.position + randomWalk;
-            Vector3 force = Toolbox.Instance.RandomVector(baseDir, 45f);
-            // force = force * ((forceMax * Random.value) + forceMin) / bitBody.mass;
+
+            Vector3 force = 2f * Toolbox.Instance.RandomVector(baseDir, 45f);
             force.z = Random.Range(0.42f, 1.05f);
             bitPhys.Add3Motion(force);
-
-            SpriteRenderer sprite = bit.GetComponent<SpriteRenderer>();
-            sprite.color = color;
-            Damageable damageable = bit.GetComponent<Damageable>();
-            if (damageable) {
-                Toolbox.Instance.DisableAndReenable(damageable, 0.2f);
-            }
         }
     }
     public static bool DamageTypeMatch(damageType dam1, damageType dam2) {
