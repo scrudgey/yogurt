5d801b409f99bf4a1992100fc443e78027f1903f
RSHAW <scrudgey@gmail.com>
2018-04-06 09:22:31 -0700

    initial commit

---
+++ generic behaviors/FlipSpriteRandom.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipSpriteRandom : MonoBehaviour {
+    public bool flipX = true;
+    public bool flipY = true;
+    public void Start() {
+        Vector3 rot = transform.localScale;
+        if (flipX) {
+            if (Random.value >= 0.5){
+                rot.x = -1f;
+            }
+        }
+        if (flipY) {
+            if (Random.value >= 0.5){
+                rot.y = -1f;
+            }
+        }
+        transform.localScale = rot;
+    }
+}
