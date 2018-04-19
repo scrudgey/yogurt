c2fbb886a5e5f356b9fdb503f7a3ac9b5b7edf1d
RSHAW <scrudgey@gmail.com>
2018-04-18 19:35:11 -0700

    simplified code

--- generic behaviors/TerminateOnStop.cs
+++ generic behaviors/TerminateOnStop.cs
@@ -1,22 +1,31 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+using System;
+
 
 public class TerminateOnStop : MonoBehaviour {
+    List<Type> typesToRemove = new List<Type>(){typeof(Rigidbody2D), typeof(Collider2D), typeof(FlipSpriteRandom)};
     void Update() {
         if (GetComponent<Rigidbody2D>().velocity.magnitude < 0.01) {
-            if (GetComponent<Rigidbody2D>())
-                Destroy(GetComponent<Rigidbody2D>());
-            if (GetComponent<Collider2D>())
-                Destroy(GetComponent<Collider2D>());
+            DestroyComponents();
             PhysicalBootstrapper pb = GetComponent<PhysicalBootstrapper>();
             if (pb){
                 pb.DestroyPhysical();
                 Destroy(pb);
             }
-                // Destroy(GetComponent<PhysicalBootstrapper>());
             SpriteRenderer renderer = GetComponent<SpriteRenderer>();
             renderer.sortingLayerName = "background";
             renderer.sortingOrder = 50;
             Destroy(this);
+        }
+    }
+    void DestroyComponents(){
+        foreach(Type type in typesToRemove){
+            Component component = GetComponent(type);
+            while (component != null){
+                DestroyImmediate(component);
+                component = GetComponent(type);
+            }
         }
     }
 }
