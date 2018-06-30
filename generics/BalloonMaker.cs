8cd2c8c520e816985a55a3a12124e6f8ccc9d896
RSHAW <scrudgey@gmail.com>
2018-06-30 16:03:05 -0700

    initial commit

---
+++ generics/BalloonMaker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonMaker : Interactive {
+    public List<GameObject> balloonPrefabs;
+	void Start () {
+		Interaction balloon = new Interaction(this, "Balloon", "MakeBalloon");
+        balloon.defaultPriority = 10;
+        balloon.hideInManualActions = false;
+        // balloon.debug = true;
+        // balloon.
+        // balloon.hideInRightClickMenu = true;
+        interactions.Add(balloon);
+	}
+	public void MakeBalloon(){
+        GameObject balloon = GameObject.Instantiate(balloonPrefabs[Random.Range(0, balloonPrefabs.Count)], transform.position, Quaternion.identity);
+    }
+}
---
+++ itemcode/BalloonRopeRenderer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BalloonRopeRenderer : MonoBehaviour {
+    public LineRenderer lineRenderer;
+    public GameObject balloon;
+    public Vector3 tieOffset;
+	void Awake () {
+		lineRenderer = GetComponent<LineRenderer>();
+        // balloon = transform.Find("/balloon").gameObject;
+        DistanceJoint2D joint = GetComponent<DistanceJoint2D>();
+        tieOffset = new Vector3(joint.connectedAnchor.x, joint.connectedAnchor.y, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 newPoint = balloon.transform.localPosition;
+        // float z = 0f;
+        float z = balloon.transform.rotation.eulerAngles.z * transform.lossyScale.x;
+        z *= Mathf.Deg2Rad;
+
+        // newPoint += tieOffset;
+
+        Vector3 offset = Vector2.zero;
+        offset.x = tieOffset.x * Mathf.Cos(z) - tieOffset.y * Mathf.Sin(z);
+        offset.y = tieOffset.x * Mathf.Sin(z) + tieOffset.y * Mathf.Cos(z);
+        newPoint += offset;
+
+        lineRenderer.SetPosition(1, newPoint);
+	}
+}
