760b3cffd0105bfce3e806ea9e2f65d45a2155ba
RSHAW <scrudgey@gmail.com>
2018-07-09 20:15:23 -0700

    initial commit

---
+++ generic behaviors/ChildPreservesScale.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildPreservesScale : MonoBehaviour {
+	void LateUpdate () {
+		if (transform.lossyScale.x < 0){
+            Vector3 scale = transform.localScale;
+            scale.x *= -1f;
+            transform.localScale = scale;
+        }
+	}
+}
