3555b7423c1a8cb6a9dbd73da89e4e67c7dd7856
RSHAW <scrudgey@gmail.com>
2018-07-25 20:22:57 -0700

    adjust usability of balloon inflate

--- generics/BalloonMaker.cs
+++ generics/BalloonMaker.cs
@@ -12,17 +12,21 @@ public class BalloonMaker : Interactive {
         Interaction balloon = new Interaction(this, "Balloon", "MakeBalloon");
         balloon.defaultPriority = 1;
         balloon.hideInManualActions = false;
+        balloon.hideInRightClickMenu = true;
+        balloon.otherOnPlayerConsent = false;
+        balloon.playerOnOtherConsent = false;
+        balloon.inertOnPlayerConsent = false;
         interactions.Add(balloon);
         audioSource = Toolbox.Instance.SetUpAudioSource(gameObject);
     }
     public void MakeBalloon() {
         Controllable controllable = GetComponent<Controllable>();
         Inventory inv = GetComponent<Inventory>();
-        if (controllable != null && inv != null){
+        if (controllable != null && inv != null) {
             StartCoroutine(BalloonRoutine(controllable, inv));
         }
     }
-    IEnumerator BalloonRoutine(Controllable controllable, Inventory inv){
+    IEnumerator BalloonRoutine(Controllable controllable, Inventory inv) {
         controllable.disabled = true;
         yield return new WaitForSeconds(0.5f);
         GameObject balloonObject = SpawnBalloon();
@@ -30,30 +34,30 @@ public class BalloonMaker : Interactive {
         if (balloonPickup != null)
             inv.GetItem(balloonPickup);
         yield return new WaitForSeconds(0.2f);
-        if (controllable == Controller.Instance.focus){
+        if (controllable == Controller.Instance.focus) {
             controllable.control = Controllable.ControlType.player;
         } else {
             controllable.control = Controllable.ControlType.AI;
         }
         controllable.disabled = false;
     }
-    public GameObject SpawnBalloon(){
+    public GameObject SpawnBalloon() {
         GameObject balloon = GameObject.Instantiate(balloonPrefabs[Random.Range(0, balloonPrefabs.Count)], transform.position, Quaternion.identity);
         SpriteRenderer spriteRenderer = balloon.GetComponent<SpriteRenderer>();
-        if (spriteRenderer != null){
+        if (spriteRenderer != null) {
             spriteRenderer.color = balloonColors[Random.Range(0, balloonColors.Count)];
         }
         Transform subBalloon = balloon.transform.Find("balloon");
-        if (subBalloon){
+        if (subBalloon) {
             spriteRenderer = subBalloon.GetComponent<SpriteRenderer>();
-            if (spriteRenderer != null){
+            if (spriteRenderer != null) {
                 spriteRenderer.color = balloonColors[Random.Range(0, balloonColors.Count)];
             }
         }
-        if (spawnSounds.Count > 0){
+        if (spawnSounds.Count > 0) {
             audioSource.PlayOneShot(spawnSounds[Random.Range(0, spawnSounds.Count)]);
         }
-        if (particleEffect != null){
+        if (particleEffect != null) {
             GameObject.Instantiate(particleEffect, transform.position, Quaternion.identity);
         }
         return balloon;
