95b6cc0324f54d06dcdca43f197523e9a2f29a9e
RSHAW <scrudgey@gmail.com>
2018-08-18 19:57:18 -0700

    initial commit

---
+++ generic behaviors/Qualities.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Qualities : MonoBehaviour {
+    public float disgusting;
+    public float disturbing;
+    public float chaos;
+    public float offensive;
+    public float positive;
+    //     public string whatHappened;
+    // public string transcriptLine;
+    // public string noun;
+    public EventData ToEvent() {
+        EventData data = new EventData(
+            disgusting:disgusting,
+            disturbing:disturbing,
+            chaos:chaos,
+            offensive:offensive,
+            positive:positive
+        );
+        data.whatHappened = "I saw "+Toolbox.Instance.GetName(gameObject);
+        data.noun = "observation";
+        return data;
+    }
+}
