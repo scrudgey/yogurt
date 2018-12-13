98c3bb11e604b7da5b4ba9177292ccadf37fc194
RSHAW <scrudgey@gmail.com>
2018-12-12 17:26:12 -0800

    initial commit

---
+++ UI/AchievementEntry.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchievementEntry : MonoBehaviour {
+    public Achievement achievement;
+    public AchievementBrowser browser;
+    public void ButtonClickedCallback(){
+        browser.AchievementEntryCallback(achievement);
+    }
+}
