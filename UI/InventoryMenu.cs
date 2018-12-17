d1f02f6478735ab51782131285c8465dedb3ae04
RSHAW <scrudgey@gmail.com>
2018-12-16 17:41:30 -0800

    initial commit

---
+++ UI/InventoryMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryMenu : MonoBehaviour {
+    public void Initialize(Inventory inventory){
+        Transform itemDrawer = transform.Find("menu/itemdrawer");
+        foreach (GameObject item in inventory.items) {
+            GameObject button = Instantiate(Resources.Load("UI/ItemButton")) as GameObject;
+            button.transform.SetParent(itemDrawer.transform, false);
+            button.GetComponent<ItemButtonScript>().SetButtonAttributes(item, inventory);
+        }
+    }
+    public void CloseButtonCallback(){
+        UINew.Instance.CloseActiveMenu();
+    }
+}
---
+++ UI/StatsBrowser.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+    // secondsPlayed,
+    // yogurtEaten,
+    // vomit,
+    // yogurtVomit,
+    // dollarsFlushed,
+    // dollarsDuplicated,
+    // dollarsBurned,
+    // swordsEaten,
+    // hatsEaten,
+    // immolations,
+    // selfImmolations,
+    // deathByCombat,
+    // deathByMisadventure,
+    // deathByAsphyxiation,
+    // mayorsSassed,
+    // actsOfCannibalism,
+    // heartsEaten,
+    // nullifications
+public class StatsBrowser : MonoBehaviour {
+    public Dictionary<StatType, string> statDescriptions = new Dictionary<StatType, string>(){
+        {StatType.secondsPlayed, "seconds played"},
+        {StatType.yogurtEaten, "yogurts eaten"},
+        {StatType.vomit, "number of times vomited"},
+        {StatType.yogurtVomit, "number of times vomited yogurt"},
+        {StatType.dollarsFlushed, "dollars flushed down the toilet"},
+        {StatType.dollarsDuplicated, "dollars duplicated"},
+        {StatType.dollarsBurned, "dollars burned"},
+        {StatType.swordsEaten, "swords eaten"},
+        {StatType.hatsEaten, "hats eaten"},
+        {StatType.immolations, "times immolated"},
+        {StatType.selfImmolations, "self immolations"},
+        {StatType.deathByCombat, "death by combat"},
+        {StatType.deathByMisadventure, "death by misadventure"},
+        {StatType.deathByAsphyxiation, "death by asphyxiation"},
+        {StatType.mayorsSassed, "mayors sassed"},
+        {StatType.actsOfCannibalism, "acts of cannibalism"},
+        {StatType.heartsEaten, "hearts eaten"},
+        {StatType.nullifications, "self nullifications"}
+    };
+    public Text title;
+    public Text content;
+    public Text count;
+    public StartMenu startMenu;
+    public void Initialize(GameData data){
+        content.text = "";
+        count.text = "";
+        foreach(KeyValuePair<StatType, string> kvp in statDescriptions){
+            content.text = content.text + kvp.Value.ToString() + "\n";
+            if (data.stats.ContainsKey(kvp.Key)){
+                int value = (int)data.stats[kvp.Key].value;
+                count.text = count.text + value.ToString() + "\n";
+            } else {
+                count.text = count.text + "0\n";
+            }
+        }
+    }
+    public void CloseButtonCallback(){
+        startMenu.CloseStatsBrowser();
+    }
+}
