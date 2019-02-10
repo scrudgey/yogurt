6f65c45aa5c0b0586b6ece66e48e40010427ba4e
RSHAW <scrudgey@gmail.com>
2019-02-10 11:57:09 -0800

    changes to slash impact hitbox

--- itemcode/Slasher.cs
+++ itemcode/Slasher.cs
@@ -4,15 +4,22 @@ public class Slasher : MonoBehaviour {
     public MessageDamage message;
     void SlashOn() {
         Vector3 startPoint = transform.position;
-        startPoint.x += message.force.normalized.x / 4f;
-        startPoint.y += message.force.normalized.y / 4f;
+        // startPoint.x += message.force.normalized.x / 4f;
+        // startPoint.y += message.force.normalized.y / 4f;
         GameObject impactObj = GameManager.Instantiate(Resources.Load("PhysicalImpact"), startPoint, Quaternion.identity) as GameObject;
+        float angle = Toolbox.Instance.ProperAngle(message.force.x, message.force.y);
+        impactObj.transform.RotateAround(impactObj.transform.position, new Vector3(0, 0, 1), angle);
         PhysicalImpact impact = impactObj.GetComponent<PhysicalImpact>();
+        impact.useBoxCollider = true;
         impact.size = 0.11f;
         impact.message = message;
-        CircleCollider2D impactCollider = impact.GetComponent<CircleCollider2D>();
-        foreach (Collider2D responsibleCollider in message.responsibleParty.GetComponentsInChildren<Collider2D>())
-            Physics2D.IgnoreCollision(impactCollider, responsibleCollider, true);
+        CircleCollider2D circleCollider = impact.GetComponent<CircleCollider2D>();
+        BoxCollider2D boxCollider = impact.GetComponent<BoxCollider2D>();
+        foreach (Collider2D responsibleCollider in message.responsibleParty.GetComponentsInChildren<Collider2D>()){
+            Physics2D.IgnoreCollision(circleCollider, responsibleCollider, true);
+            Physics2D.IgnoreCollision(boxCollider, responsibleCollider, true);
+        }
+        // Debug.Break();
     }
     void SlashOff() {
         // currently empty but referred to in animation
