26d22b3d7e7353d6cb87f6da04a2ce83e51d9c54
RSHAW <scrudgey@gmail.com>
2019-02-10 11:56:31 -0800

    fire causes no damage, only ignites flammables

--- itemcode/Fire.cs
+++ itemcode/Fire.cs
@@ -1,61 +1,63 @@
 ﻿using UnityEngine;
-// using System.Collections;
 using System.Collections.Generic;
 
 public class Fire : MonoBehaviour {
     public Flammable flammable;
     private Dictionary<GameObject, Flammable> flammables = new Dictionary<GameObject, Flammable>();
     private MessageDamage message;
-    private HashSet<GameObject> damageQueue = new HashSet<GameObject>();
-    private HashSet<GameObject> collidedObjects = new HashSet<GameObject>();
-    private float damageTimer;
+    // private HashSet<GameObject> damageQueue = new HashSet<GameObject>();
+    // private HashSet<GameObject> collidedObjects = new HashSet<GameObject>();
+    // private float damageTimer;
     static List<string> forbiddenTags = new List<string>(new string[] { "occurrenceFlag", "background", "sightcone" });
     void Start() {
         message = new MessageDamage(1f, damageType.fire);
     }
-    void Update() {
-        if (flammable)
-            if (flammable.noDamage)
-                return;
-        damageTimer += Time.deltaTime;
-        if (damageTimer > 0.05f) {
-            damageTimer = 0;
-            if (flammable)
-                message.responsibleParty = flammable.responsibleParty;
+    // void Update() {
+        // if (flammable)
+        //     if (flammable.noDamage)
+        //         return;
+        // damageTimer += Time.deltaTime;
+        // if (damageTimer > 0.05f) {
+        //     damageTimer = 0;
+            // if (flammable)
+            //     message.responsibleParty = flammable.responsibleParty;
             // process the objects in the damage queue.
             // do not send a damage message to anything above me in the transform tree: 
             // this is so that the player can hold a flaming object without being hurt.
-            foreach (GameObject obj in collidedObjects){
-                if (flammables.ContainsKey(obj)) {
-                    Flammable targetFlam = flammables[obj];
-                    targetFlam.heat += Time.deltaTime * 2f;
-                    if (flammable.responsibleParty != null) {
-                        targetFlam.responsibleParty = flammable.responsibleParty;
-                    }
-                }
-            }
-            collidedObjects = new HashSet<GameObject>();
-        }
-        foreach (GameObject obj in damageQueue) {
-                if (obj == null)
-                    continue;
-                if (transform.IsChildOf(obj.transform.root))
-                    continue;
-                if (obj != null) {
-                    Toolbox.Instance.SendMessage(obj, this, message);
-                }
-            }
-        damageQueue = new HashSet<GameObject>();
+            // foreach (GameObject obj in collidedObjects){
+            //     if (flammables.ContainsKey(obj)) {
+            //         Flammable targetFlam = flammables[obj];
+            //         targetFlam.heat += Time.deltaTime * 2f;
+            //         if (flammable.responsibleParty != null) {
+            //             targetFlam.responsibleParty = flammable.responsibleParty;
+            //         }
+            //     }
+            // }
+            // collidedObjects = new HashSet<GameObject>();
+        // }
+        // foreach (GameObject obj in damageQueue) {
+        //         if (obj == null)
+        //             continue;
+        //         if (transform.IsChildOf(obj.transform.root))
+        //             continue;
+        //         if (obj != null) {
+        //             Toolbox.Instance.SendMessage(obj, this, message);
+        //         }
+        //     }
+        // damageQueue = new HashSet<GameObject>();
         
-    }
+    // }
+    // void Update(){
+    //     _transforms = (Transform[])transform.root.GetComponentsInChildren( typeof(Transform), true);
+    // }
     void OnTriggerEnter2D(Collider2D coll) {
         if (!flammables.ContainsKey(coll.gameObject)) {
             Flammable flam = coll.GetComponentInParent<Flammable>();
+            Fire otherFire = coll.GetComponentInParent<Fire>();
             if (flam != null && flam != flammable) {
                 flammables.Add(coll.gameObject, flam);
                 return;
             }
-            Fire otherFire = coll.GetComponentInParent<Fire>();
             if (otherFire != null && otherFire != this){
                 flammables.Add(coll.gameObject, otherFire.flammable);
             }
@@ -67,13 +69,17 @@ public class Fire : MonoBehaviour {
             return;
         if (forbiddenTags.Contains(coll.tag))
             return;
+        if (coll.transform.IsChildOf(transform.root))
+            return;
+        // if (transform.root.)
         // Debug.Log(coll.gameObject);
-        collidedObjects.Add(coll.gameObject);
-        damageQueue.Add(coll.transform.root.gameObject);
-        if (flammables.ContainsKey(coll.gameObject)) {
-            flammables[coll.gameObject].heat += Time.deltaTime;
-            if (flammable.responsibleParty != null) {
-                flammables[coll.gameObject].responsibleParty = flammable.responsibleParty;
+        // collidedObjects.Add(coll.gameObject);
+        // damageQueue.Add(coll.transform.root.gameObject);
+        Flammable flam = null;
+        if (flammables.TryGetValue(coll.gameObject, out flam)){
+            flam.heat += Time.deltaTime;
+            if (flam.responsibleParty != null) {
+                flam.responsibleParty = flammable.responsibleParty;
             }
         }
     }
