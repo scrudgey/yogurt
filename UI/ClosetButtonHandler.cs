046c6b99a5b144706127bb84336b1a00e319e78e
RSHAW <scrudgey@gmail.com>
2019-03-24 14:43:42 -0700

    update to closet menu prefab

--- UI/ClosetButtonHandler.cs
+++ UI/ClosetButtonHandler.cs
@@ -1,17 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 using UnityEngine.UI;
+using System.Linq;
 
 public class ClosetButtonHandler : MonoBehaviour {
-    private Image icon;
-    private Text titleText;
-    private Text descriptionText;
-    private Text nameText;
+    public Image icon;
+    public Text titleText;
+    public Text descriptionText;
+    public Text nameText;
+    public Transform listContent;
+    // public Text titleText;
     void Start() {
         GetComponent<Canvas>().worldCamera = GameManager.Instance.cam;
-        icon = transform.Find("menu/body/InfoPanel/ImagePanel/Icon").GetComponent<Image>();
-        descriptionText = transform.Find("menu/body/InfoPanel/TextPanel/Description").GetComponent<Text>();
-        nameText = transform.Find("menu/body/InfoPanel/TextPanel/Title").GetComponent<Text>();
+        // icon = transform.Find("menu/body/InfoPanel/ImagePanel/Icon").GetComponent<Image>();
+        // descriptionText = transform.Find("menu/body/InfoPanel/TextPanel/Description").GetComponent<Text>();
+        // nameText = transform.Find("menu/body/InfoPanel/TextPanel/Title").GetComponent<Text>();
         icon.sprite = null;
         icon.color = new Color(1f, 1f, 1f, 0f);
     }
@@ -20,9 +23,13 @@ public class ClosetButtonHandler : MonoBehaviour {
         return newObject.GetComponent<ItemEntryScript>();
     }
     public void PopulateItemList(HomeCloset.ClosetType type) {
-        GameObject listObject = transform.Find("menu/body/ItemList").gameObject;
+        foreach (Transform childObject in listContent) {
+            Destroy(childObject.gameObject);
+        }
+        // GameObject listObject = transform.Find("menu/body/ItemList").gameObject;
         List<string> itemList = GameManager.Instance.data.collectedObjects;
-        titleText = transform.Find("menu/menubar/titlebar").GetComponent<Text>();
+
+        // titleText = transform.Find("menu/menubar/titlebar").GetComponent<Text>();
         if (type == HomeCloset.ClosetType.clothing) {
             itemList = GameManager.Instance.data.collectedClothes;
             titleText.text = "Collected Clothing";
@@ -33,10 +40,21 @@ public class ClosetButtonHandler : MonoBehaviour {
             itemList = GameManager.Instance.data.collectedItems;
             titleText.text = "Collected Items";
         }
+        Dictionary<string, string> names = new Dictionary<string, string>();
+        foreach (string name in itemList) {
+            GameObject tempObject = Instantiate(Resources.Load("prefabs/" + name)) as GameObject;
+            Item tempItem = tempObject.GetComponent<Item>();
+            // names.Add(tempItem.itemName);
+            names[name] = tempItem.itemName;
+            Destroy(tempObject);
+        }
+        // docs = docs.OrderBy(d => docsIds.IndexOf(d.Id)).ToList();
+        // itemList.Sort();
+        itemList = itemList.OrderBy(i => names[i]).ToList();
         foreach (string name in itemList) {
             ItemEntryScript script = spawnEntry();
             script.Configure(name, type);
-            script.transform.SetParent(listObject.transform, false);
+            script.transform.SetParent(listContent, false);
         }
     }
     public void CloseButtonClick() {
