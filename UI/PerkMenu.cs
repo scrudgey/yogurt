38b337d541db649c0333784ded403ee735c7d818
RSHAW <scrudgey@gmail.com>
2019-03-24 19:17:31 -0700

    sort perks

--- UI/PerkMenu.cs
+++ UI/PerkMenu.cs
@@ -32,30 +32,38 @@ public class PerkMenu : MonoBehaviour {
             .Cast<GameObject>()
             .ToArray();
         int numberCollected = 0;
-        foreach(KeyValuePair<string, bool> kvp in GameManager.Instance.data.perks){
-            if (kvp.Value){
+        foreach (KeyValuePair<string, bool> kvp in GameManager.Instance.data.perks) {
+            if (kvp.Value) {
                 numberCollected += 1;
             }
         }
+        Dictionary<GameObject, PerkComponent> perkComponents = new Dictionary<GameObject, PerkComponent>();
         foreach (GameObject prefab in perkPrefabs) {
             PerkComponent component = prefab.GetComponent<PerkComponent>();
-            if (component) {
-                GameObject buttonObject = Instantiate(Resources.Load("UI/PerkButton")) as GameObject;
-                Text buttonText = buttonObject.transform.Find("Text").GetComponent<Text>();
-                buttonObject.transform.SetParent(buttonList, false);
-                PerkButton perkScript = buttonObject.GetComponent<PerkButton>();
-                perkScript.menu = this;
-                perkScript.perk = new Perk(component.perk);
-                buttonText.text = component.perk.title;
-                Button perkButton = buttonObject.GetComponent<Button>();
-                if (numberCollected < component.perk.requiredPerks){
-                    perkButton.interactable = false;
-                }
-                if (GameManager.Instance.data.perks[component.perk.name]) {
-                    perkButton.interactable = false;
-                }
-                
+            if (component)
+                perkComponents[prefab] = component;
+        }
+        perkPrefabs = perkPrefabs.OrderBy(p => perkComponents[p].perk.requiredPerks).ToArray();
+        foreach (GameObject prefab in perkPrefabs) {
+            PerkComponent component = perkComponents[prefab];
+
+            GameObject buttonObject = Instantiate(Resources.Load("UI/PerkButton")) as GameObject;
+            Text buttonText = buttonObject.transform.Find("Text").GetComponent<Text>();
+            buttonObject.transform.SetParent(buttonList, false);
+            PerkButton perkScript = buttonObject.GetComponent<PerkButton>();
+            perkScript.menu = this;
+            perkScript.perk = new Perk(component.perk);
+            buttonText.text = component.perk.title;
+            Button perkButton = buttonObject.GetComponent<Button>();
+            if (numberCollected < component.perk.requiredPerks) {
+                perkButton.interactable = false;
             }
+            if (GameManager.Instance.data.perks[component.perk.name]) {
+                perkButton.interactable = false;
+            }
+        }
+        if (numberCollected == perkPrefabs.Count()) {
+            doneButton.interactable = true;
         }
     }
     public void PerkButtonClicked(PerkButton buttonScript) {
@@ -74,7 +82,7 @@ public class PerkMenu : MonoBehaviour {
         doneButton.interactable = true;
         GameManager.Instance.data.perks[selectedPerk.perk.name] = true;
         selectedPerk.GetComponent<Button>().interactable = false;
-        UINew.Instance.RefreshUI(active:true);
+        UINew.Instance.RefreshUI(active: true);
         UINew.Instance.PlayUISound("sounds/8-bit/BOUNCE3");
     }
     public void DoneButtonClick() {
