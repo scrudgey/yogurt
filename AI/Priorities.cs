eb0267d6335bfb79bedabf369faaf009e64f9578
RSHAW <scrudgey@gmail.com>
2019-08-07 21:05:00 -0700

    added vampire trapdoor priority

--- AI/Priorities.cs
+++ AI/Priorities.cs
@@ -157,11 +157,33 @@ namespace AI {
         }
         public override float Urgency(Personality personality) {
             if (zone.bounds.Contains(gameObject.transform.position)) {
+                urgency = Priority.urgencyMinor;
                 return Priority.urgencyMinor;
             } else {
                 urgency = Priority.urgencySmall;
                 return Priority.urgencySmall;
             }
+        }
+    }
+    public class PriorityTrapdoor : Priority {
+        public Ref<Vector2> standPoint;
+        public Ref<GameObject> player;
+        public PriorityTrapdoor(GameObject g, Controllable c, Vector3 guardPoint) : base(g, c) {
+            priorityName = "trapdoor";
+            player = new Ref<GameObject>(GameManager.Instance.playerObject);
+            Goal getToZone = new GoalWalkToPoint(gameObject, control, new Ref<Vector2>((Vector2)guardPoint));
+            Goal lookGoal = new GoalLookInDirection(gameObject, control, Vector2.down);
+            Goal observe = new GoalObserveObject(gameObject, control, player);
+            Goal trap = new GoalTriggerTrapdoor(gameObject, control);
+            lookGoal.requirements.Add(getToZone);
+            observe.requirements.Add(lookGoal);
+            trap.requirements.Add(observe);
+            goal = trap;
+        }
+        public override float Urgency(Personality personality) {
+            player.val = GameManager.Instance.playerObject;
+            urgency = Priority.urgencySmall;
+            return Priority.urgencySmall;
         }
     }
     public class PrioritySentry : Priority {
