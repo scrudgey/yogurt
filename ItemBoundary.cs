1d48d176cd92c90b3ed03307d0843cda2adb91ad
RSHAW <scrudgey@gmail.com>
2019-10-06 15:53:22 -0700

    initial commit

---
+++ ItemBoundary.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBoundary : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ generic behaviors/Blink.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blink : MonoBehaviour {
+    public List<Behaviour> blinkers = new List<Behaviour>();
+    public float timer;
+    public float onInterval;
+    public float offInterval;
+    private bool blinkersOn;
+    void Update() {
+        timer += Time.unscaledDeltaTime;
+        if ((timer > onInterval && blinkersOn) || (timer > offInterval && !blinkersOn)) {
+            timer = 0;
+            blinkersOn = !blinkersOn;
+            foreach (Behaviour blinker in blinkers) {
+                blinker.enabled = blinkersOn;
+            }
+        }
+    }
+}
