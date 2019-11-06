1e4f92059b830dbaa3ef342194973415463193dd
RSHAW <scrudgey@gmail.com>
2019-11-05 19:05:19 -0800

    remove broken import

--- singletons/Toolbox.cs
+++ singletons/Toolbox.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
-using Realtime.Messaging.Internal;
 public enum SkinColor {
     light,
     dark,
