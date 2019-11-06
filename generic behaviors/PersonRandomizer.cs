e9d3f4c01a0bbfa8e3d2dc781f0708356b6c05a8
RSHAW <scrudgey@gmail.com>
2019-11-05 18:55:45 -0800

    initial commit

---
+++ generic behaviors/PersonRandomizer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonRandomizer : MonoBehaviour, ISaveable {
+    public bool randomizeGender;
+    public bool randomizeSkinColor;
+    public bool randomizeDirection;
+    public List<Uniform> randomOutfits;
+    private bool configured = false;
+    void LateUpdate() {
+        if (configured)
+            return;
+        if (randomOutfits != null) {
+            Outfit outfit = GetComponent<Outfit>();
+            Uniform uniform = GameObject.Instantiate(randomOutfits[Random.Range(0, randomOutfits.Count)]);
+            GameObject removedUniform = outfit.DonUniform(uniform, cleanStains: false);
+            if (removedUniform)
+                Destroy(removedUniform);
+        }
+        if (randomizeGender) {
+            Gender newGender = (Gender)Random.Range(0, 2);
+            Toolbox.SetGender(gameObject, newGender);
+        }
+        if (randomizeDirection) {
+            Vector2 newDirection = Random.insideUnitCircle.normalized;
+            Controllable controllable = GetComponent<Controllable>();
+            controllable.direction = newDirection;
+        }
+        configured = true;
+    }
+
+    public void SaveData(PersistentComponent data) {
+        data.bools["configured"] = configured;
+    }
+    public void LoadData(PersistentComponent data) {
+        configured = data.bools["configured"];
+    }
+}
---
+++ generics/Grabbable.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabbable : Interactive {
+    public GameObject itemPrefab;
+    public bool fire;
+    // Use this for initialization
+    void Start() {
+        Interaction getAction = new Interaction(this, "Get", "Get", true, false);
+        interactions.Add(getAction);
+    }
+
+    // Update is called once per frame
+    public void Get(Inventory inventory) {
+        GameObject item = GameObject.Instantiate(itemPrefab, transform.position, Quaternion.identity);
+        if (fire) {
+            Flammable itemFlammable = item.GetComponent<Flammable>();
+            if (itemFlammable) {
+                itemFlammable.heat += 100f;
+                itemFlammable.burnTimer = 1f;
+                itemFlammable.fireRetardantBuffer = 0f;
+                itemFlammable.onFire = true;
+            }
+        }
+        inventory.GetItem(item.GetComponent<Pickup>());
+        Destroy(gameObject);
+    }
+}
